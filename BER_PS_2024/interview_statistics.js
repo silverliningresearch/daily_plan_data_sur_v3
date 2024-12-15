3e31ead46defce67ee8a7c30d3c52304f4ea45bd
unknown <lb@silverliningresearch.com>
2024-12-15 13:55:26 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "15-12-2024 14:38:45"
+        "download_time": "15-12-2024 14:53:39"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -15903,7 +15903,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2024-12-15",
         "quota_id": "FCO - Ryanair",
-        "Number of interviews": 3
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2024-12-15",
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "15-12-2024 16:41:06"
+        "download_time": "15-12-2024 16:55:20"
     },
     {
         "InterviewDate": "2024-10-08",
@@ -4619,6 +4619,11 @@ let interview_statistics = `[
         "InterviewDate": "2024-12-15",
         "quota_id": "IX-HYD",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-12-15",
+        "quota_id": "SV-KHI",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2024-12-15",
