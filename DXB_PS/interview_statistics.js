e8d8b1a593e1d1beadf85bd16f19bb65609076b1
unknown <lb@silverliningresearch.com>
2024-12-21 21:31:24 +0000

    update new data

--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "22-12-2024 01:12:01"
+        "download_time": "22-12-2024 01:29:10"
     },
     {
         "InterviewDate": "2024-01-07",
@@ -7455,6 +7455,14 @@ let interview_statistics = `[
         "DestCode": "MXP",
         "AirlineCode": "EK",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-12-22",
+        "quota_id": "EK366",
+        "Flight": "EK366",
+        "DestCode": "TPE",
+        "AirlineCode": "EK",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2024-12-22",
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "21-12-2024 22:12:46"
+        "download_time": "21-12-2024 22:29:28"
     },
     {
         "InterviewDate": "2024-01-28",
