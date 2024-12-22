c175e5831d2aaf89e6d8e81a2140fc7b8562c453
unknown <lb@silverliningresearch.com>
2024-12-22 19:56:03 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "22-12-2024 23:43:52"
+        "download_time": "22-12-2024 23:55:47"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -14488,7 +14488,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 11
+        "Number of interviews": 13
     },
     {
         "InterviewDate": "2024-12-22",
@@ -14523,6 +14523,13 @@ let interview_statistics = `[
         "quota_id": "5-5",
         "Survey": 5,
         "Location": 2,
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-12-22",
+        "quota_id": "6-6",
+        "Survey": 6,
+        "Location": 1,
         "Number of interviews": 1
     },
     {
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "22-12-2024 20:40:00"
+        "download_time": "22-12-2024 20:54:18"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -37396,7 +37396,7 @@ let interview_statistics = `[
         "Interview_Date": "2024-12-22",
         "Dest": "DOH",
         "AirlineCode": "QR",
-        "completed_interviews": 1
+        "completed_interviews": 13
     },
     {
         "Interview_Date": "2024-12-22",
