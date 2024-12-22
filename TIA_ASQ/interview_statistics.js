68bea5774ee3be101e11aa054597de7f87e29af6
unknown <lb@silverliningresearch.com>
2024-12-22 19:31:45 +0000

    update new data

--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "22-12-2024 20:12:44"
+        "download_time": "22-12-2024 20:29:48"
     },
     {
         "InterviewDate": "2024-01-28",
--- VIE_ADV/interview_statistics.js
+++ VIE_ADV/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AF-CDG",
         "Number of interviews": 10,
-        "download_time": "22-12-2024 20:12:22"
+        "download_time": "22-12-2024 20:29:42"
     },
     {
         "InterviewDate": "2024-01-03",
