a268931420981444f219cb77fab975c00fef8ff4
unknown <lb@silverliningresearch.com>
2024-12-28 23:32:21 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "29-12-2024 00:10:40"
+        "download_time": "29-12-2024 00:27:10"
     },
     {
         "InterviewDate": "2024-01-03",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "29-12-2024 03:12:56"
+        "download_time": "29-12-2024 03:30:10"
     },
     {
         "InterviewDate": "2024-01-07",
