2e09e3ed095ef875d7183c03fe2f3cd957e3b2a2
unknown <lb@silverliningresearch.com>
2024-12-29 23:44:15 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "30-12-2024 07:29:27"
+        "download_time": "30-12-2024 07:41:50"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -14377,8 +14377,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2024-12-30",
-        "quota_id": "Tiffany & Co.\u2666TERMINAL 1\u2666Airside\u2666#02-45\u2666LX\u2666Min8",
+        "quota_id": "Christian Dior\u2666TERMINAL 1\u2666Airside\u2666#02-44 & #03-44-MZ\u2666LX\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-12-30",
+        "quota_id": "Tiffany & Co.\u2666TERMINAL 1\u2666Airside\u2666#02-45\u2666LX\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "30-12-2024 02:32:43"
+        "download_time": "30-12-2024 02:44:11"
     },
     {
         "InterviewDate": "2024-10-08",
