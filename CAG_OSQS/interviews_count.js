dc8628c48c71aa1e891e8535c163221d6f3d039f
unknown <lb@silverliningresearch.com>
2024-12-30 02:44:57 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "30-12-2024 10:29:00"
+        "download_time": "30-12-2024 10:40:55"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -14378,7 +14378,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2024-12-30",
         "quota_id": "Christian Dior\u2666TERMINAL 1\u2666Airside\u2666#02-44 & #03-44-MZ\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2024-12-30",
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "30-12-2024 03:28:14"
+        "download_time": "30-12-2024 03:40:26"
     },
     {
         "Interview_Date": "2023-05-28",
