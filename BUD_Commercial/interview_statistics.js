d757b52c06be0a9ebce5d6f150e51709cc246388
unknown <lb@silverliningresearch.com>
2024-12-31 17:59:14 +0000

    update new data

--- BUD_Commercial/interview_statistics.js
+++ BUD_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "BCN-FR",
         "Number of interviews": 3,
-        "download_time": "31-12-2024 18:40:52"
+        "download_time": "31-12-2024 18:54:58"
     },
     {
         "InterviewDate": "2024-01-02",
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "31-12-2024 18:42:55"
+        "download_time": "31-12-2024 18:57:02"
     },
     {
         "Interview_Date": "2023-05-28",
