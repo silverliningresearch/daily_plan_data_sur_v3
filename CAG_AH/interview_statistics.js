aff33ccd1a924e9a8f56d2f4494837592c001b6c
unknown <lb@silverliningresearch.com>
2024-12-31 01:31:57 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "31-12-2024 09:09:41"
+        "download_time": "31-12-2024 09:25:54"
     },
     {
         "Interview_Date": "2023-12-18",
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "31-12-2024 04:16:30"
+        "download_time": "31-12-2024 04:31:54"
     },
     {
         "InterviewDate": "2024-10-08",
