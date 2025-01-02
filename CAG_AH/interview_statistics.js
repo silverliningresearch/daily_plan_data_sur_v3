c97069e95f11f476f0e32ad6b626706ad4aac24b
unknown <lb@silverliningresearch.com>
2025-01-02 10:14:35 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "02-01-2025 17:54:46"
+        "download_time": "02-01-2025 18:09:24"
     },
     {
         "Interview_Date": "2023-12-18",
@@ -34198,7 +34198,7 @@ let interview_statistics = `[
         "Interview_Date": "2025-01-02",
         "Dest": "BKK",
         "AirlineCode": "SQ",
-        "completed_interviews": 8
+        "completed_interviews": 10
     },
     {
         "Interview_Date": "2025-01-02",
@@ -34360,7 +34360,7 @@ let interview_statistics = `[
         "Interview_Date": "2025-01-02",
         "Dest": "SZX",
         "AirlineCode": "ZH",
-        "completed_interviews": 2
+        "completed_interviews": 6
     },
     {
         "Interview_Date": "2025-01-02",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-08-30",
         "quota_id": "Beyond The Vines\u2666TERMINAL 1\u2666Airside\u2666#02-K25\u2666MP\u2666Min8",
         "Number of interviews": 4,
-        "download_time": "02-01-2025 17:56:00"
+        "download_time": "02-01-2025 18:10:57"
     },
     {
         "InterviewDate": "2024-08-30",
@@ -14503,7 +14503,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-02",
         "quota_id": "Hermes\u2666TERMINAL 1\u2666Airside\u2666#02-55/56\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2025-01-02",
@@ -14528,7 +14528,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-02",
         "quota_id": "Tory Burch\u2666TERMINAL 1\u2666Airside\u2666#02-57\u2666LX\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
