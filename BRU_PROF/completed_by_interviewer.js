6ecd7d073c120b6b945e591ef27d6d3aff88757b
unknown <lb@silverliningresearch.com>
2025-01-05 10:54:49 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "05-01-2025 11:39:07"
+        "download_time": "05-01-2025 11:53:01"
     },
     {
         "InterviewerID": 1,
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "05-01-2025 18:40:57"
+        "download_time": "05-01-2025 18:53:49"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -623,7 +623,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-05",
         "quota_id": "Tech House\u2666TERMINAL 3\u2666Landside\u2666#B2-K3\u2666LS\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2025-01-05",
