c07ca91fdc8b4566ca622d2bf26afedfb0c27f86
unknown <lb@silverliningresearch.com>
2025-01-07 01:41:16 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "07-01-2025 09:25:38"
+        "download_time": "07-01-2025 09:39:51"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -733,7 +733,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-07",
         "quota_id": "Lacoste\u2666TERMINAL 2\u2666Airside\u2666#02-163 (#026-098-01)\u2666MP\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
