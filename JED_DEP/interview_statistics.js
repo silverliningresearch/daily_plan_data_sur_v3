dd10aa65972b1b05def5072a313f2a494cf08764
unknown <lb@silverliningresearch.com>
2025-01-12 01:30:15 +0000

    update new data

--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "12-01-2025 04:12:12"
+        "download_time": "12-01-2025 04:29:48"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -5098,7 +5098,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-01-12",
         "quota_id": "QR-DOH",
-        "Number of interviews": 3
+        "Number of interviews": 6
     }
 ]    
 
