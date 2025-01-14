51adb8c6d695e781979a1c1247910eb4a12b7526
unknown <lb@silverliningresearch.com>
2025-01-14 21:55:37 +0000

    update new data

--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "14-01-2025 22:39:54"
+        "download_time": "14-01-2025 22:54:12"
     },
     {
         "Interview_Date": "2023-05-28",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "15-01-2025 01:40:54"
+        "download_time": "15-01-2025 01:55:05"
     },
     {
         "InterviewDate": "2024-01-07",
