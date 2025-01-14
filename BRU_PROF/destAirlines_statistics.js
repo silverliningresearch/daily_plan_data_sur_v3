18f66b3e0375aa8da92eeeee78bfae0214e93b4c
unknown <lb@silverliningresearch.com>
2025-01-14 22:43:05 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "14-01-2025 23:24:30"
+        "download_time": "14-01-2025 23:38:43"
     },
     {
         "InterviewDate": "2023-04-01",
--- DXB_PS/interview_statistics.js
+++ DXB_PS/interview_statistics.js
@@ -6,7 +6,7 @@ let interview_statistics = `[
         "DestCode": "KWI",
         "AirlineCode": "J9",
         "Number of interviews": 2,
-        "download_time": "15-01-2025 02:28:03"
+        "download_time": "15-01-2025 02:41:49"
     },
     {
         "InterviewDate": "2024-01-07",
