3445b44c79918cafb40d753ec2d940a1af87f5a0
unknown <lb@silverliningresearch.com>
2025-01-14 02:46:18 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "14-01-2025 10:26:28"
+        "download_time": "14-01-2025 10:44:32"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -1592,8 +1592,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2025-01-14",
+        "quota_id": "CHANGI RECOMMENDS\u2666TERMINAL 4\u2666Landside\u2666#01-K11\u2666CR\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2025-01-14",
         "quota_id": "CHEERS\u2666TERMINAL 4\u2666Landside\u2666#01-09\u2666LS\u2666Min8",
-        "Number of interviews": 8
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2025-01-14",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "14-01-2025 05:29:24"
+        "download_time": "14-01-2025 05:45:55"
     },
     {
         "InterviewDate": "2024-10-01",
