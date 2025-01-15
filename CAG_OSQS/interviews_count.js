a5eb2ae61fa1fbd3187d1de440310d5dddcdc103
unknown <lb@silverliningresearch.com>
2025-01-15 02:42:52 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "15-01-2025 10:26:47"
+        "download_time": "15-01-2025 10:40:38"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -1788,7 +1788,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-15",
         "quota_id": "Sprint-Cass (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-186\u2666EL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2025-01-15",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "15-01-2025 05:29:50"
+        "download_time": "15-01-2025 05:42:21"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -5293,7 +5293,12 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-01-15",
         "quota_id": "NE-CAI",
-        "Number of interviews": 2
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-01-15",
+        "quota_id": "XY-GIZ",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-01-15",
