90d6bdfc776a6cc2dae0c215326917fa26d636a5
unknown <lb@silverliningresearch.com>
2025-01-18 23:29:27 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "19-01-2025 07:10:57"
+        "download_time": "19-01-2025 07:25:31"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -2217,8 +2217,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2025-01-19",
+        "quota_id": "Old Seng Choong\u2666TERMINAL 4\u2666Airside\u2666#02-50\u2666CCD\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-01-19",
         "quota_id": "WH Smith - Premises J (Air)\u2666TERMINAL 4\u2666Airside\u2666#02-59\u2666OW\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 4
     }
 ]    
 
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "19-01-2025 00:10:27"
+        "download_time": "19-01-2025 00:24:53"
     },
     {
         "Interview_Date": "2023-05-28",
