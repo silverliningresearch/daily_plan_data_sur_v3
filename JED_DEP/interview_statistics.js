1c36fcbc3af3dd4a02680129f1d5341af2508b7e
unknown <lb@silverliningresearch.com>
2025-01-19 23:43:25 +0000

    update new data

--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "20-01-2025 02:29:54"
+        "download_time": "20-01-2025 02:42:41"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -5459,6 +5459,11 @@ let interview_statistics = `[
         "InterviewDate": "2025-01-18",
         "quota_id": "OV-MCT",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2025-01-20",
+        "quota_id": "EK-DXB",
+        "Number of interviews": 1
     }
 ]    
 
