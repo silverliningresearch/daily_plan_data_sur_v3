b73ae86cf120ca87ba2cc52f1ff427ff433975b4
unknown <lb@silverliningresearch.com>
2025-01-21 03:30:40 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "21-01-2025 11:09:33"
+        "download_time": "21-01-2025 11:25:01"
     },
     {
         "Interview_Date": "2023-12-18",
@@ -36572,6 +36572,12 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2025-01-21",
+        "Dest": "DPS",
+        "AirlineCode": "QZ",
+        "completed_interviews": 2
+    },
+    {
+        "Interview_Date": "2025-01-21",
         "Dest": "FRA",
         "AirlineCode": "SQ",
         "completed_interviews": 7
@@ -36592,7 +36598,7 @@ let interview_statistics = `[
         "Interview_Date": "2025-01-21",
         "Dest": "SUB",
         "AirlineCode": "3K",
-        "completed_interviews": 3
+        "completed_interviews": 4
     },
     {
         "Interview_Date": "2025-01-21",
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T4_3K_DPS",
         "interviewerID": "03",
         "Number of interviews": 3,
-        "download_time": "21-01-2025 11:12:27"
+        "download_time": "21-01-2025 11:28:06"
     },
     {
         "InterviewDate": "01-01-2025",
@@ -1624,7 +1624,7 @@ let interview_statistics  = `[
         "InterviewDate": "21-01-2025",
         "quota_id": "T4_QZ_DPS",
         "interviewerID": "04",
-        "Number of interviews": 5
+        "Number of interviews": 6
     }
 ]    
 
