89b0eb2f6e6695bf8d21157c7fdacee101665e41
unknown <lb@silverliningresearch.com>
2025-01-23 06:31:55 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "23-01-2025 14:10:21"
+        "download_time": "23-01-2025 14:26:57"
     },
     {
         "Interview_Date": "2023-12-18",
@@ -36832,13 +36832,13 @@ let interview_statistics = `[
         "Interview_Date": "2025-01-23",
         "Dest": "CGK",
         "AirlineCode": "SQ",
-        "completed_interviews": 1
+        "completed_interviews": 2
     },
     {
         "Interview_Date": "2025-01-23",
         "Dest": "CNX",
         "AirlineCode": "TR",
-        "completed_interviews": 3
+        "completed_interviews": 7
     },
     {
         "Interview_Date": "2025-01-23",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "23-01-2025 14:11:27"
+        "download_time": "23-01-2025 14:29:07"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -2874,6 +2874,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-01-23",
         "quota_id": "T2 iSC Departure counter (beside GST Refund Counter)\u2666TERMINAL 2\u2666Airside\u2666NA\u2666PS\u2666Min8",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2025-01-23",
+        "quota_id": "The Cocoa Trees (Premises B3) (T2 DTN, near E20)\u2666TERMINAL 2\u2666Airside\u2666#02-403\u2666CCD\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-01-23",
