68df829700f096ac8d53ea53a176389f736bef16
unknown <lb@silverliningresearch.com>
2025-01-24 18:15:43 +0000

    update new data

--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "24-01-2025 18:53:50"
+        "download_time": "24-01-2025 19:11:46"
     },
     {
         "InterviewDate": "2023-04-01",
@@ -93003,7 +93003,7 @@ let destAirlines_statistics  = `[
     {
         "InterviewDate": "2025-01-24",
         "quota_id": "MXP-SN",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2025-01-24",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "24-01-2025 18:55:43"
+        "download_time": "24-01-2025 19:14:50"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -76,7 +76,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-01-24",
         "InterviewerID": "10 - Asser Z.",
         "Q0B": "COMWELL COPENHAGEN PORTSIDE",
-        "completes": 9
+        "completes": 10
     },
     {
         "InterviewDate": "2025-01-24",
