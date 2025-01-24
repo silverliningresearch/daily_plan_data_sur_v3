f50d098ded23e9e4beee2ed16de44429517e8b41
unknown <lb@silverliningresearch.com>
2025-01-24 00:30:23 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "24-01-2025 08:11:21"
+        "download_time": "24-01-2025 08:26:25"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -2973,7 +2973,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-24",
         "quota_id": "KOPITIAM\u2666TERMINAL 4\u2666Landside\u2666#02-205\u2666FBL\u2666Min25",
-        "Number of interviews": 7
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2025-01-24",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "24-01-2025 01:13:05"
+        "download_time": "24-01-2025 01:29:28"
     },
     {
         "InterviewDate": "2025-01-14",
