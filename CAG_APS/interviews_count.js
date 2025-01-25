cc5816de5447d0db0eb617116aeffc2074f01bf4
unknown <lb@silverliningresearch.com>
2025-01-25 01:28:12 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T4_3K_DPS",
         "interviewerID": "03",
         "Number of interviews": 3,
-        "download_time": "25-01-2025 09:13:29"
+        "download_time": "25-01-2025 09:27:38"
     },
     {
         "InterviewDate": "01-01-2025",
@@ -1925,6 +1925,12 @@ let interview_statistics  = `[
         "quota_id": "T1_CA_PEK",
         "interviewerID": "02",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "25-01-2025",
+        "quota_id": "T1_QR_DOH",
+        "interviewerID": "02",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "25-01-2025",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "25-01-2025 09:12:26"
+        "download_time": "25-01-2025 09:26:31"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -3198,12 +3198,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-25",
         "quota_id": "Oakley\u2666TERMINAL 2\u2666Airside\u2666#02-244\u2666MP\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2025-01-25",
         "quota_id": "iStudio\u2666TERMINAL 2\u2666Airside\u2666#02-238\u2666EL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
