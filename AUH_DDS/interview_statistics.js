946f2a57d9fe1b6b32ad958aa6111a94d7476db5
unknown <lb@silverliningresearch.com>
2025-01-25 19:12:37 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "25-01-2025 22:56:33"
+        "download_time": "25-01-2025 23:12:19"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -16469,7 +16469,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 1,
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2025-01-25",
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "25-01-2025 19:54:43"
+        "download_time": "25-01-2025 20:10:32"
     },
     {
         "Interview_Date": "2023-05-28",
