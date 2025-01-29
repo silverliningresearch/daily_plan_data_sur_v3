80adcbc647c602b88199dce8cb430409479a7208
unknown <lb@silverliningresearch.com>
2025-01-29 06:41:41 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "29-01-2025 14:27:34"
+        "download_time": "29-01-2025 14:40:16"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -3759,6 +3759,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-01-29",
         "quota_id": "Perfumes & Cosmetics (T2 ABS)\u2666TERMINAL 2\u2666Airside\u2666#01-177\u2666PC\u2666Min15",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-01-29",
+        "quota_id": "Perfumes & Cosmetics (T2 DTC)\u2666TERMINAL 2\u2666Airside\u2666#02-174 (#026-103E)\u2666PC\u2666Min25",
+        "Number of interviews": 1
     }
 ]    
 
