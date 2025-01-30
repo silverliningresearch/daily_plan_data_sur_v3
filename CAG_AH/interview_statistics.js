b676b7c85e78ea47798fd70565c0e0c575150e6d
unknown <lb@silverliningresearch.com>
2025-01-30 01:29:31 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "30-01-2025 09:10:45"
+        "download_time": "30-01-2025 09:25:24"
     },
     {
         "Interview_Date": "2023-12-18",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "30-01-2025 02:13:39"
+        "download_time": "30-01-2025 02:29:30"
     },
     {
         "InterviewDate": "2025-01-14",
