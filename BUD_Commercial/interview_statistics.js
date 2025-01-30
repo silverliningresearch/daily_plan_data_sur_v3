e1a81ce5e944e7ce49d062936295f3afa4ddb7e2
unknown <lb@silverliningresearch.com>
2025-01-30 20:55:52 +0000

    update new data

--- BUD_Commercial/interview_statistics.js
+++ BUD_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "BCN-FR",
         "Number of interviews": 3,
-        "download_time": "30-01-2025 21:39:47"
+        "download_time": "30-01-2025 21:53:39"
     },
     {
         "InterviewDate": "2024-01-02",
