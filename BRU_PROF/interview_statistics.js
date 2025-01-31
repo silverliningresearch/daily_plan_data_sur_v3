dd57a4e47adb6cba48c56d80c0deb930c24a4f00
unknown <lb@silverliningresearch.com>
2025-01-31 21:15:37 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "31-01-2025 21:53:39"
+        "download_time": "31-01-2025 22:10:42"
     },
     {
         "InterviewDate": "2023-04-01",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "31-01-2025 21:55:59"
+        "download_time": "31-01-2025 22:15:34"
     },
     {
         "InterviewDate": "2025-01-14",
