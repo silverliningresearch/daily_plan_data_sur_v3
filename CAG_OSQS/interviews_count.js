60160aa3a10dc5783f4dc86f715b525a49cd7b6c
unknown <lb@silverliningresearch.com>
2025-01-31 03:29:23 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "31-01-2025 11:12:23"
+        "download_time": "31-01-2025 11:27:12"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -3873,6 +3873,16 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-01-30",
         "quota_id": "XW Western Grill \u2666TERMINAL 2\u2666Landside\u2666#03-23\u2666FBL\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-01-31",
+        "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 2\u2666Airside\u2666#03-175/176\u2666HL\u2666Min20",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-01-31",
+        "quota_id": "Ambassador Transit Hotel\u2666TERMINAL 3\u2666Airside\u2666#03-15\u2666HL\u2666Min20",
         "Number of interviews": 1
     },
     {
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "31-01-2025 06:13:40"
+        "download_time": "31-01-2025 06:28:37"
     },
     {
         "InterviewDate": "2024-10-01",
