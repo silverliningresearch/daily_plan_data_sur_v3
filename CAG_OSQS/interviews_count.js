f1394a1e6546da5c76efec45dbf6e54597259067
unknown <lb@silverliningresearch.com>
2025-02-01 09:30:46 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "01-02-2025 17:12:53"
+        "download_time": "01-02-2025 17:27:46"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -3977,13 +3977,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2025-02-01",
+        "quota_id": "ORCHID CAFETERIA\u2666TERMINAL 1\u2666Landside\u2666#B1-12A\u2666FBL\u2666Min25",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-02-01",
         "quota_id": "Prosegur T1 Arr Mtg Hall East\u2666TERMINAL 1\u2666Landside\u2666#01-K27\u2666MC\u2666Min8",
         "Number of interviews": 8
     },
     {
         "InterviewDate": "2025-02-01",
         "quota_id": "Prosegur T1 Arr Mtg Hall West\u2666TERMINAL 1\u2666Landside\u2666#01-K25\u2666MC\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2025-02-01",
