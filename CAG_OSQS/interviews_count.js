fe7507b39b6553b2c0f84dad90fd7dea38e7e987
unknown <lb@silverliningresearch.com>
2025-02-01 11:30:23 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "01-02-2025 19:14:01"
+        "download_time": "01-02-2025 19:27:42"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -3979,6 +3979,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-02-01",
         "quota_id": "ORCHID CAFETERIA\u2666TERMINAL 1\u2666Landside\u2666#B1-12A\u2666FBL\u2666Min25",
         "Number of interviews": 11
+    },
+    {
+        "InterviewDate": "2025-02-01",
+        "quota_id": "Premises G (Jewel) - Baggage Storage by Smarte Carte\u2666TERMINAL 1\u2666Landside\u2666#01-302\u2666BS\u2666Min8",
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2025-02-01",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "01-02-2025 12:16:16"
+        "download_time": "01-02-2025 12:29:53"
     },
     {
         "InterviewDate": "2025-01-14",
