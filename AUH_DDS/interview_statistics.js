cacc342c06f66a10d251039e3a98c0dbb9941413
unknown <lb@silverliningresearch.com>
2025-02-02 12:30:01 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "02-02-2025 16:15:20"
+        "download_time": "02-02-2025 16:29:25"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "02-02-2025 20:12:37"
+        "download_time": "02-02-2025 20:25:48"
     },
     {
         "Interview_Date": "2023-12-18",
@@ -37252,7 +37252,7 @@ let interview_statistics = `[
         "Interview_Date": "2025-02-02",
         "Dest": "KUL",
         "AirlineCode": "TR",
-        "completed_interviews": 1
+        "completed_interviews": 2
     },
     {
         "Interview_Date": "2025-02-02",
