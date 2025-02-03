c3739e1af63219883366ec19142696deccb9e3da
unknown <lb@silverliningresearch.com>
2025-02-03 00:57:12 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "03-02-2025 08:39:22"
+        "download_time": "03-02-2025 08:54:25"
     },
     {
         "Interview_Date": "2023-12-18",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "03-02-2025 08:40:19"
+        "download_time": "03-02-2025 08:55:22"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -4078,7 +4078,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-02-03",
         "quota_id": "Burger King\u2666TERMINAL 3\u2666Airside\u2666#03-13\u2666FBA\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2025-02-03",
