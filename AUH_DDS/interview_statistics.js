513af753880450a053558d21c333fca491277758
unknown <lb@silverliningresearch.com>
2025-02-05 22:55:43 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "06-02-2025 02:40:16"
+        "download_time": "06-02-2025 02:55:24"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "06-02-2025 06:38:45"
+        "download_time": "06-02-2025 06:53:49"
     },
     {
         "Interview_Date": "2023-12-18",
@@ -37787,6 +37787,12 @@ let interview_statistics = `[
         "Dest": "NRT",
         "AirlineCode": "TR",
         "completed_interviews": 5
+    },
+    {
+        "Interview_Date": "2025-02-06",
+        "Dest": "SUB",
+        "AirlineCode": "SQ",
+        "completed_interviews": 1
     }
 ]    
 
