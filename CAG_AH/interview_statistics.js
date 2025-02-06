38cd1b25cee51008543a4ce8bf4beb4da67d07d6
unknown <lb@silverliningresearch.com>
2025-02-06 22:57:17 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "07-02-2025 06:39:44"
+        "download_time": "07-02-2025 06:54:45"
     },
     {
         "Interview_Date": "2023-12-18",
@@ -37880,6 +37880,12 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2025-02-07",
+        "Dest": "BOM",
+        "AirlineCode": "SQ",
+        "completed_interviews": 2
+    },
+    {
+        "Interview_Date": "2025-02-07",
         "Dest": "CAN",
         "AirlineCode": "CZ",
         "completed_interviews": 6
@@ -37894,7 +37900,7 @@ let interview_statistics = `[
         "Interview_Date": "2025-02-07",
         "Dest": "DPS",
         "AirlineCode": "TR",
-        "completed_interviews": 2
+        "completed_interviews": 5
     },
     {
         "Interview_Date": "2025-02-07",
