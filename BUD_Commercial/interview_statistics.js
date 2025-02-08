122fb3c3c74a5abf84657a75adbd0d911d6ddf34
unknown <lb@silverliningresearch.com>
2025-02-08 15:13:28 +0000

    update new data

--- BUD_Commercial/interview_statistics.js
+++ BUD_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "BCN-FR",
         "Number of interviews": 3,
-        "download_time": "08-02-2025 15:53:16"
+        "download_time": "08-02-2025 16:09:08"
     },
     {
         "InterviewDate": "2024-01-02",
@@ -6634,6 +6634,11 @@ let interview_statistics = `[
         "InterviewDate": "2025-02-08",
         "quota_id": "LPL-FR",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-02-08",
+        "quota_id": "STN-FR",
+        "Number of interviews": 4
     }
 ]    
 
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "08-02-2025 17:54:54"
+        "download_time": "08-02-2025 18:13:04"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -6233,7 +6233,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-02-08",
         "quota_id": "QR-DOH",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-02-08",
@@ -6242,8 +6242,13 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2025-02-08",
+        "quota_id": "KU-KWI",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-02-08",
         "quota_id": "SV-RUH",
-        "Number of interviews": 10
+        "Number of interviews": 11
     }
 ]    
 
