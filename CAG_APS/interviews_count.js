16b2ffc4dfa40b96380816643471f70527350c10
unknown <lb@silverliningresearch.com>
2025-02-08 02:42:29 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T4_3K_DPS",
         "interviewerID": "03",
         "Number of interviews": 3,
-        "download_time": "08-02-2025 10:29:21"
+        "download_time": "08-02-2025 10:41:37"
     },
     {
         "InterviewDate": "01-01-2025",
