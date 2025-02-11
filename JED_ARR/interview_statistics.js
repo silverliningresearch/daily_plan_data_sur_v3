f00ee5cf11d3d0fd25817e3fc9fc6d24506c5fdf
unknown <lb@silverliningresearch.com>
2025-02-11 19:54:53 +0000

    update new data

--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "11-02-2025 22:39:33"
+        "download_time": "11-02-2025 22:54:50"
     },
     {
         "InterviewDate": "2024-10-08",
@@ -8203,7 +8203,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-02-11",
         "quota_id": "F3-TUU",
-        "Number of interviews": 1
+        "Number of interviews": 2
     }
 ]    
 
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "11-02-2025 20:39:18"
+        "download_time": "11-02-2025 20:54:33"
     },
     {
         "InterviewDate": "2024-01-28",
