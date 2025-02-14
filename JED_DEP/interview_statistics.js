e828ea90c4e82af5b9d4848777a1fcb6f7f56f12
unknown <lb@silverliningresearch.com>
2025-02-14 15:30:43 +0000

    update new data

--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "14-02-2025 18:13:30"
+        "download_time": "14-02-2025 18:30:12"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -6514,6 +6514,11 @@ let interview_statistics = `[
         "InterviewDate": "2025-02-14",
         "quota_id": "XY-DMM",
         "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2025-02-14",
+        "quota_id": "SV-MED",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-02-14",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "14-02-2025 16:13:47"
+        "download_time": "14-02-2025 16:30:40"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -446,6 +446,12 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2025-02-14",
+        "InterviewerID": "15 - Bjarne J.",
+        "Q0B": "WAKEUP AARHUS",
+        "completes": 1
+    },
+    {
+        "InterviewDate": "2025-02-14",
         "InterviewerID": "17 - Naja H.",
         "Q0B": "R\u00d8DVIG KRO",
         "completes": 4
@@ -466,7 +472,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-02-14",
         "InterviewerID": "8 - Sofie H.",
         "Q0B": "SCANDIC HOTEL COPENHAGEN",
-        "completes": 2
+        "completes": 3
     }
 ]    
 
