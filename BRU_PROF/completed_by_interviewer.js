e34f1d935424d0ab4379b92920697f47361196e1
unknown <lb@silverliningresearch.com>
2025-02-16 03:28:42 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "16-02-2025 04:08:51"
+        "download_time": "16-02-2025 04:24:47"
     },
     {
         "InterviewerID": 1,
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "16-02-2025 04:13:12"
+        "download_time": "16-02-2025 04:28:07"
     },
     {
         "InterviewDate": "2025-01-14",
