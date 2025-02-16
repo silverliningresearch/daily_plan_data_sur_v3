30bb13b6448e68c0dc8188a46a0065ce1d9e8486
unknown <lb@silverliningresearch.com>
2025-02-16 02:55:25 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "16-02-2025 06:40:13"
+        "download_time": "16-02-2025 06:55:06"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -17701,7 +17701,7 @@ let interview_statistics = `[
         "quota_id": "7-7",
         "Survey": 7,
         "Location": 8888888,
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "16-02-2025 10:39:26"
+        "download_time": "16-02-2025 10:54:19"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -5543,6 +5543,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-02-16",
         "quota_id": "GUARDIAN HEALTH & BEAUTY\u2666TERMINAL 3\u2666Landside\u2666#B2-24\u2666LS\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2025-02-16",
+        "quota_id": "MIXSHOP\u2666TERMINAL 3\u2666Landside\u2666#B2-32A\u2666LS\u2666Min8",
         "Number of interviews": 2
     },
     {
