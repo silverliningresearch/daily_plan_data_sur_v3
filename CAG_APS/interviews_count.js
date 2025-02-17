eaa18a1a886f13721ac6e0882bcb52499ef896d1
unknown <lb@silverliningresearch.com>
2025-02-17 01:58:05 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T2_AC_YVR",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "17-02-2025 09:40:04"
+        "download_time": "17-02-2025 09:56:45"
     },
     {
         "InterviewDate": "01-02-2025",
@@ -1271,6 +1271,12 @@ let interview_statistics  = `[
         "quota_id": "T1_CZ_CAN",
         "interviewerID": "02",
         "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "17-02-2025",
+        "quota_id": "T1_TK_MEL",
+        "interviewerID": "02",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "17-02-2025",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "17-02-2025 09:39:27"
+        "download_time": "17-02-2025 09:55:57"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -5644,6 +5644,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-02-16",
         "quota_id": "WH Smith - Premises C (Land) (T1 Landside)\u2666TERMINAL 1\u2666Landside\u2666#02-84\u2666OW\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2025-02-17",
+        "quota_id": "Be Relax\u2666TERMINAL 3\u2666Airside\u2666#02-32\u2666OS\u2666Min8",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-02-17",
