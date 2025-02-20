e32eaba62829721e9d4afefe8f3c0036f2260e8c
unknown <lb@silverliningresearch.com>
2025-02-20 04:13:29 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "20-02-2025 11:53:34"
+        "download_time": "20-02-2025 12:11:19"
     },
     {
         "Interview_Date": "2023-12-18",
@@ -39437,6 +39437,12 @@ let interview_statistics = `[
         "Dest": "FRA",
         "AirlineCode": "SQ",
         "completed_interviews": 7
+    },
+    {
+        "Interview_Date": "2025-02-20",
+        "Dest": "HKG",
+        "AirlineCode": "CX",
+        "completed_interviews": 1
     },
     {
         "Interview_Date": "2025-02-20",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "20-02-2025 06:54:58"
+        "download_time": "20-02-2025 07:13:19"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -6887,13 +6887,18 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2025-02-20",
+        "quota_id": "F3-RUH",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-02-20",
         "quota_id": "SV-RUH",
         "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-02-20",
         "quota_id": "XY-RUH",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-02-20",
