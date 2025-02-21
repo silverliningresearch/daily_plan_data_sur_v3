17ab270c72cf8ca4d2ecc97afbbbc660f40962c9
unknown <lb@silverliningresearch.com>
2025-02-21 11:14:08 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "SQ",
         "completed_interviews": 4,
-        "download_time": "21-02-2025 18:53:37"
+        "download_time": "21-02-2025 19:11:10"
     },
     {
         "Interview_Date": "2023-12-18",
@@ -39524,6 +39524,12 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2025-02-21",
+        "Dest": "KCH",
+        "AirlineCode": "TR",
+        "completed_interviews": 2
+    },
+    {
+        "Interview_Date": "2025-02-21",
         "Dest": "KUL",
         "AirlineCode": "AK",
         "completed_interviews": 10
@@ -39562,7 +39568,7 @@ let interview_statistics = `[
         "Interview_Date": "2025-02-21",
         "Dest": "SYD",
         "AirlineCode": "QF",
-        "completed_interviews": 9
+        "completed_interviews": 10
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "21-02-2025 18:54:11"
+        "download_time": "21-02-2025 19:12:12"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -6383,7 +6383,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-02-21",
         "quota_id": "LT T3 DT North\u2666TERMINAL 3\u2666Airside\u2666#02-65\u2666LT\u2666Min15",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-02-21",
@@ -6404,6 +6404,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-02-21",
         "quota_id": "STARHUB\u2666TERMINAL 4\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2025-02-21",
+        "quota_id": "Travelex T3 DT South\u2666TERMINAL 3\u2666Airside\u2666#02-K7T\u2666MC\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-02-21",
