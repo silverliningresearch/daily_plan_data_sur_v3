cd4ba03567f837b14207ee62522b88780d298834
unknown <lb@silverliningresearch.com>
2025-02-22 02:31:06 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T2_AC_YVR",
         "interviewerID": "02",
         "Number of interviews": 3,
-        "download_time": "22-02-2025 10:12:47"
+        "download_time": "22-02-2025 10:29:02"
     },
     {
         "InterviewDate": "01-02-2025",
@@ -1814,6 +1814,12 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "22-02-2025",
+        "quota_id": "T3_GA_DPS",
+        "interviewerID": "02",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "22-02-2025",
         "quota_id": "T3_QR_DOH",
         "interviewerID": "02",
         "Number of interviews": 1
@@ -1840,7 +1846,13 @@ let interview_statistics  = `[
         "InterviewDate": "22-02-2025",
         "quota_id": "T4_3K_BKK",
         "interviewerID": "04",
-        "Number of interviews": 1
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "22-02-2025",
+        "quota_id": "T4_AK_KCH",
+        "interviewerID": "04",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "22-02-2025",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "22-02-2025 05:13:45"
+        "download_time": "22-02-2025 05:30:11"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -6969,6 +6969,11 @@ let interview_statistics = `[
         "InterviewDate": "2025-02-22",
         "quota_id": "SV-EAM",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-02-22",
+        "quota_id": "F3-RUH",
+        "Number of interviews": 1
     }
 ]    
 
