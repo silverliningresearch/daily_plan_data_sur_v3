9027c5324e3b49b6a1fa800d911b392d41fc7663
unknown <lb@silverliningresearch.com>
2025-02-22 03:14:25 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "22-02-2025 06:55:03"
+        "download_time": "22-02-2025 07:13:26"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -18037,14 +18037,14 @@ let interview_statistics = `[
         "quota_id": "1-1",
         "Survey": 1,
         "Location": 8888888,
-        "Number of interviews": 19
+        "Number of interviews": 20
     },
     {
         "InterviewDate": "2025-02-22",
         "quota_id": "2-2",
         "Survey": 2,
         "Location": 8888888,
-        "Number of interviews": 9
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "2025-02-22",
@@ -18058,13 +18058,20 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 2,
-        "Number of interviews": 5
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2025-02-22",
         "quota_id": "5-5",
         "Survey": 5,
         "Location": 1,
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-02-22",
+        "quota_id": "5-5",
+        "Survey": 5,
+        "Location": 2,
         "Number of interviews": 1
     },
     {
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "22-02-2025 03:55:22"
+        "download_time": "22-02-2025 04:13:59"
     },
     {
         "InterviewDate": "2025-01-14",
