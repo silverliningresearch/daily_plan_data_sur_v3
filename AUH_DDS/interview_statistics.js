fd769efb43a9968a80f088b4634844daf7bcc79a
unknown <lb@silverliningresearch.com>
2025-02-24 15:28:54 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "24-02-2025 19:11:59"
+        "download_time": "24-02-2025 19:28:28"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -18170,7 +18170,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 1,
-        "Number of interviews": 11
+        "Number of interviews": 12
     },
     {
         "InterviewDate": "2025-02-24",
@@ -18205,7 +18205,7 @@ let interview_statistics = `[
         "quota_id": "7-7",
         "Survey": 7,
         "Location": 8888888,
-        "Number of interviews": 8
+        "Number of interviews": 9
     }
 ]    
 
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "24-02-2025 16:09:50"
+        "download_time": "24-02-2025 16:26:47"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -38771,6 +38771,12 @@ let interview_statistics = `[
         "Dest": "VIE",
         "AirlineCode": "FR",
         "completed_interviews": 5
+    },
+    {
+        "Interview_Date": "2025-02-24",
+        "Dest": "LGW",
+        "AirlineCode": "D8",
+        "completed_interviews": 9
     }
 ]    
 
