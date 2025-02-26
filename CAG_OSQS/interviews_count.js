ea30bf928f7547650acb5dd941d39281dce6e21b
unknown <lb@silverliningresearch.com>
2025-02-26 23:40:35 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "27-02-2025 07:26:57"
+        "download_time": "27-02-2025 07:39:21"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -7408,7 +7408,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-02-27",
         "quota_id": "Aerotel\u2666TERMINAL 1\u2666Airside\u2666#03-139&140\u2666HL\u2666Min20",
-        "Number of interviews": 6
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2025-02-27",
@@ -7418,7 +7418,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-02-27",
         "quota_id": "Perfumes & Cosmetics (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-58\u2666PC\u2666Min15",
-        "Number of interviews": 3
+        "Number of interviews": 6
     }
 ]    
 
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "27-02-2025 00:28:34"
+        "download_time": "27-02-2025 00:40:03"
     },
     {
         "InterviewDate": "2024-01-28",
