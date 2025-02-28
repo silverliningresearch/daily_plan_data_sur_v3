6d74ad03656956c07b96ee0281b001d89250c26a
unknown <lb@silverliningresearch.com>
2025-02-28 23:41:08 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "01-03-2025 07:27:45"
+        "download_time": "01-03-2025 07:39:53"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -7642,8 +7642,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2025-03-01",
+        "quota_id": "Bengawan Solo\u2666TERMINAL 1\u2666Airside\u2666#02-15N\u2666CCD\u2666Min8",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-03-01",
         "quota_id": "TWG Tea Boutique\u2666TERMINAL 1\u2666Airside\u2666#02-14\u2666CCD\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     }
 ]    
 
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "01-03-2025 00:27:04"
+        "download_time": "01-03-2025 00:39:36"
     },
     {
         "Interview_Date": "2023-05-28",
