d49bd7463dda091a5fd4465cf6bb7aa17ad04c91
unknown <lb@silverliningresearch.com>
2025-03-04 19:42:27 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "04-03-2025 20:27:06"
+        "download_time": "04-03-2025 20:40:37"
     },
     {
         "InterviewDate": "2024-01-03",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "04-03-2025 22:31:03"
+        "download_time": "04-03-2025 22:42:15"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -7328,7 +7328,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-03-04",
         "quota_id": "MS-CAI",
-        "Number of interviews": 11
+        "Number of interviews": 12
     },
     {
         "InterviewDate": "2025-03-04",
