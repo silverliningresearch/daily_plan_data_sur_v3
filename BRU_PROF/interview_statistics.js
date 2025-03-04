20a5ebffec6aeaff5bf6d828009c5bec30a41e38
unknown <lb@silverliningresearch.com>
2025-03-04 12:43:02 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "04-03-2025 13:24:04"
+        "download_time": "04-03-2025 13:38:57"
     },
     {
         "InterviewDate": "2023-04-01",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "04-03-2025 13:29:43"
+        "download_time": "04-03-2025 13:43:01"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -802,7 +802,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-04",
         "InterviewerID": "24 - Kasper R.",
         "Q0B": "DGI-BYENS HOTEL",
-        "completes": 4
+        "completes": 5
     },
     {
         "InterviewDate": "2025-03-04",
