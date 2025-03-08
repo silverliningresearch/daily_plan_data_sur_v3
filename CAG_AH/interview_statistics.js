db20f47ecd9ed4f62c22ad404a247d93184f107d
unknown <lb@silverliningresearch.com>
2025-03-08 21:32:35 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "TPE",
         "AirlineCode": "SQ",
         "completed_interviews": 1,
-        "download_time": "09-03-2025 05:14:57"
+        "download_time": "09-03-2025 05:26:52"
     },
     {
         "Interview_Date": "2025-01-01",
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "08-03-2025 22:17:58"
+        "download_time": "08-03-2025 22:30:47"
     },
     {
         "InterviewDate": "2024-01-28",
