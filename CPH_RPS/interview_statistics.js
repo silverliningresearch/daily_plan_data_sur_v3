99c0e7bbc93b7817c0ab030c52d49c6bc3d198df
unknown <lb@silverliningresearch.com>
2025-03-09 22:42:15 +0000

    update new data

--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "09-03-2025 23:29:09"
+        "download_time": "09-03-2025 23:40:28"
     },
     {
         "Interview_Date": "2023-05-28",
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "10-03-2025 01:32:23"
+        "download_time": "10-03-2025 01:42:07"
     },
     {
         "InterviewDate": "2024-10-08",
