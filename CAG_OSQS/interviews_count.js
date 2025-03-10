8e2268f22e83a9ba0d9875c499b3ef8800822b70
unknown <lb@silverliningresearch.com>
2025-03-10 06:15:12 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "10-03-2025 13:55:02"
+        "download_time": "10-03-2025 14:13:12"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -8508,12 +8508,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-10",
         "quota_id": "TWG Tea Company (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-26\u2666CCD\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-03-10",
         "quota_id": "The Cocoa Trees (Premises C1) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-27\u2666CCD\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     }
 ]    
 
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "10-03-2025 08:56:12"
+        "download_time": "10-03-2025 09:14:43"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -7604,6 +7604,11 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-10",
         "quota_id": "IX-HYD",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-03-10",
+        "quota_id": "J9-KWI",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-03-10",
