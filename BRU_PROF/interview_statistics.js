6f521b21c093b5f43eafa4320074cd9ee7bfb839
unknown <lb@silverliningresearch.com>
2025-03-11 20:43:29 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "11-03-2025 21:25:55"
+        "download_time": "11-03-2025 21:39:03"
     },
     {
         "InterviewDate": "2023-04-01",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "11-03-2025 23:31:50"
+        "download_time": "11-03-2025 23:43:14"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -7627,6 +7627,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2025-03-11",
+        "quota_id": "XY-ADD",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-03-11",
         "quota_id": "F3-AHB",
         "Number of interviews": 1
     },
@@ -7684,6 +7689,11 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-11",
         "quota_id": "XY-RUH",
         "Number of interviews": 19
+    },
+    {
+        "InterviewDate": "2025-03-11",
+        "quota_id": "SV-SIN",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-03-11",
