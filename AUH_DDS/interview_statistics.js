5628cde88e4797ce7c176a8358796faca2cc6c38
unknown <lb@silverliningresearch.com>
2025-03-14 05:44:43 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "14-03-2025 09:30:57"
+        "download_time": "14-03-2025 09:44:27"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "14-03-2025 13:29:24"
+        "download_time": "14-03-2025 13:43:39"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -9058,7 +9058,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-14",
         "quota_id": "CHANGI MEET & GREET\u2666TERMINAL 4\u2666Landside\u2666#01-K3\u2666CR\u2666Min8",
-        "Number of interviews": 4
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2025-03-14",
@@ -9073,7 +9073,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-14",
         "quota_id": "EU YAN SANG\u2666TERMINAL 2\u2666Landside\u2666#02-14\u2666LS\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2025-03-14",
@@ -9083,7 +9083,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-14",
         "quota_id": "Jones The Grocer\u2666TERMINAL 2\u2666Landside\u2666#02-01\u2666FBL\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-03-14",
@@ -9103,7 +9103,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-14",
         "quota_id": "STARHUB\u2666TERMINAL 4\u2666Landside\u2666#01-K4\u2666CR\u2666Min8",
-        "Number of interviews": 6
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2025-03-14",
