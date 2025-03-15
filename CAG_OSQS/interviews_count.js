4e0d69429f6dc988ba3eb7a6e5a16927abf57970
unknown <lb@silverliningresearch.com>
2025-03-15 03:42:43 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "15-03-2025 11:28:27"
+        "download_time": "15-03-2025 11:41:36"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -9198,12 +9198,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-15",
         "quota_id": "Eu Yan Sang\u2666TERMINAL 2\u2666Airside\u2666#02-246\u2666PH\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2025-03-15",
         "quota_id": "Hugo Boss\u2666TERMINAL 2\u2666Airside\u2666#02-232\u2666LX\u2666Min8",
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2025-03-15",
@@ -9213,7 +9213,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-15",
         "quota_id": "Marc Jacobs\u2666TERMINAL 2\u2666Airside\u2666#02-227\u2666MP\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-03-15",
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 11,
-        "download_time": "15-03-2025 04:27:47"
+        "download_time": "15-03-2025 04:41:26"
     },
     {
         "Interview_Date": "2024-02-28",
