47b01fbb24cc19df816d896ac7bb3980486aa5d2
unknown <lb@silverliningresearch.com>
2025-03-15 23:44:39 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "TPE",
         "AirlineCode": "SQ",
         "completed_interviews": 1,
-        "download_time": "16-03-2025 07:26:42"
+        "download_time": "16-03-2025 07:39:11"
     },
     {
         "Interview_Date": "2025-01-01",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "16-03-2025 07:31:59"
+        "download_time": "16-03-2025 07:42:30"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -9334,6 +9334,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-03-16",
         "quota_id": "BURGER KING\u2666TERMINAL 1\u2666Landside\u2666#03-23\u2666FBL\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2025-03-16",
+        "quota_id": "ORCHID CAFETERIA\u2666TERMINAL 1\u2666Landside\u2666#B1-12A\u2666FBL\u2666Min25",
+        "Number of interviews": 3
     }
 ]    
 
