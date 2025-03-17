ba0d6abcc9e0c2d32e42373ed1a57854d359fb68
unknown <lb@silverliningresearch.com>
2025-03-17 17:57:07 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "17-03-2025 21:43:05"
+        "download_time": "17-03-2025 21:56:49"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -19458,7 +19458,7 @@ let interview_statistics = `[
         "quota_id": "4-4",
         "Survey": 4,
         "Location": 2,
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-03-17",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "17-03-2025 18:43:23"
+        "download_time": "17-03-2025 18:57:06"
     },
     {
         "InterviewDate": "2025-01-14",
