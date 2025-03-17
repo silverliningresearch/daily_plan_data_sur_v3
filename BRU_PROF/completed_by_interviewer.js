60695bec1d05c573216e096ce5f56fcbab322dfa
unknown <lb@silverliningresearch.com>
2025-03-17 20:58:52 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "17-03-2025 21:38:16"
+        "download_time": "17-03-2025 21:53:20"
     },
     {
         "InterviewerID": 1,
--- BUD_Commercial/interview_statistics.js
+++ BUD_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "BCN-FR",
         "Number of interviews": 3,
-        "download_time": "17-03-2025 21:38:19"
+        "download_time": "17-03-2025 21:53:24"
     },
     {
         "InterviewDate": "2024-01-02",
