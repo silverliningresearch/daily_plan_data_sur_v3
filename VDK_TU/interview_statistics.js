49f7c5dedcd709faab9394afe5abd3d4aeafd3d5
unknown <lb@silverliningresearch.com>
2025-03-17 16:32:16 +0000

    update new data

--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "17-03-2025 17:20:08"
+        "download_time": "17-03-2025 17:32:13"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -1204,7 +1204,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-17",
         "InterviewerID": "24 - Kasper R.",
         "Q0B": "TIVOLI HOTEL",
-        "completes": 7
+        "completes": 8
     },
     {
         "InterviewDate": "2025-03-17",
@@ -1222,7 +1222,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-17",
         "InterviewerID": "33 - Udo Q.",
         "Q0B": "S\u00d8NDERVIG STRAND",
-        "completes": 7
+        "completes": 8
     },
     {
         "InterviewDate": "2025-03-17",
