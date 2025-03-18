c075d316e5a2d86360f8aa2994020fce71f73522
unknown <lb@silverliningresearch.com>
2025-03-18 14:34:12 +0000

    update new data

--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 11,
-        "download_time": "18-03-2025 15:15:39"
+        "download_time": "18-03-2025 15:29:27"
     },
     {
         "Interview_Date": "2024-02-28",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "18-03-2025 15:18:20"
+        "download_time": "18-03-2025 15:33:13"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -1240,7 +1240,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-18",
         "InterviewerID": "24 - Kasper R.",
         "Q0B": "COMWELL COPENHAGEN PORTSIDE",
-        "completes": 4
+        "completes": 5
     },
     {
         "InterviewDate": "2025-03-18",
@@ -1252,7 +1252,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-18",
         "InterviewerID": "33 - Udo Q.",
         "Q0B": "HVIDE SANDE STRAND",
-        "completes": 8
+        "completes": 9
     },
     {
         "InterviewDate": "2025-03-18",
