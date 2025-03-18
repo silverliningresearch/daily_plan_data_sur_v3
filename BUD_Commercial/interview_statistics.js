c06ca5173caad637da42902eec9c8a7b3328502f
unknown <lb@silverliningresearch.com>
2025-03-18 20:44:22 +0000

    update new data

--- BUD_Commercial/interview_statistics.js
+++ BUD_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "BCN-FR",
         "Number of interviews": 3,
-        "download_time": "18-03-2025 21:24:09"
+        "download_time": "18-03-2025 21:38:30"
     },
     {
         "InterviewDate": "2024-01-02",
--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "18-03-2025 21:30:28"
+        "download_time": "18-03-2025 21:43:49"
     },
     {
         "InterviewDate": "2024-01-28",
