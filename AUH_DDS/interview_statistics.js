1d678cac95541f9a52098ead09f11e11b8425f27
unknown <lb@silverliningresearch.com>
2025-03-24 05:33:03 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "24-03-2025 09:16:03"
+        "download_time": "24-03-2025 09:31:59"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -19835,6 +19835,13 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-24",
         "quota_id": "3-3",
         "Survey": 3,
+        "Location": 1,
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-03-24",
+        "quota_id": "3-3",
+        "Survey": 3,
         "Location": 2,
         "Number of interviews": 6
     },
@@ -19857,7 +19864,7 @@ let interview_statistics = `[
         "quota_id": "7-7",
         "Survey": 7,
         "Location": 8888888,
-        "Number of interviews": 1
+        "Number of interviews": 6
     }
 ]    
 
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "24-03-2025 13:14:55"
+        "download_time": "24-03-2025 13:29:59"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -10507,6 +10507,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2025-03-24",
+        "quota_id": "Asian Artistry\u2666TERMINAL 2\u2666Airside\u2666#02-153\u2666JW\u2666Min8",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-03-24",
         "quota_id": "Bee Cheng Hiang\u2666TERMINAL 4\u2666Airside\u2666#02-85\u2666CCD\u2666Min8",
         "Number of interviews": 8
     },
@@ -10538,7 +10543,12 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-24",
         "quota_id": "Metapod (T2 DTN)\u2666TERMINAL 2\u2666Airside\u2666#02-150\u2666EL\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-03-24",
+        "quota_id": "Oakley\u2666TERMINAL 4\u2666Airside\u2666#02-47\u2666MP\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-03-24",
@@ -10554,6 +10564,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-03-24",
         "quota_id": "Taste Singapore\u2666TERMINAL 4\u2666Airside\u2666#02-72\u2666CCD\u2666Min8",
         "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-03-24",
+        "quota_id": "The Fashion Place\u2666TERMINAL 4\u2666Airside\u2666#02-60/61\u2666LX\u2666Min8",
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-03-24",
