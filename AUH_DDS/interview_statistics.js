4e58a0324dbf43260dd9503bcbf21241be13148f
unknown <lb@silverliningresearch.com>
2025-03-26 03:33:18 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "26-03-2025 07:15:44"
+        "download_time": "26-03-2025 07:31:36"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -19970,6 +19970,13 @@ let interview_statistics = `[
         "Survey": 3,
         "Location": 2,
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-03-26",
+        "quota_id": "5-5",
+        "Survey": 5,
+        "Location": 2,
+        "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-03-26",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "26-03-2025 11:14:33"
+        "download_time": "26-03-2025 11:29:38"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -10903,7 +10903,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-03-26",
         "quota_id": "Foodies' Clan\u2666TERMINAL 2\u2666Landside\u2666#0610-ASC\u2666FBL\u2666Min25",
-        "Number of interviews": 4
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2025-03-26",
