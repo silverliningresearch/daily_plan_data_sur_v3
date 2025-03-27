9bae556d030b77f9ca95b88c866fcc0f81ef837d
unknown <lb@silverliningresearch.com>
2025-03-27 16:20:44 +0000

    update new data

---
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -0,0 +1,165 @@
+let interview_statistics = `[
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "6E-AMD",
+        "Number of interviews": 1,
+        "download_time": "27-03-2025 20:20:43"
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "MS-CAI",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "3L-CCU",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "5W-CLJ",
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "5W-DMM",
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "5W-GYD",
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "EY-HYD",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "PK-ISB",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "5W-KUT",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "3L-KWI",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "6E-LKO",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "5W-MED",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "EY-PVG",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "SU-SVO",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-11-26",
+        "quota_id": "W4-VIE",
+        "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "3L-AMM",
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "3L-BAH",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "EY-CCJ",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "3L-DAC",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "BS-DAC",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "5W-HBE",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "EY-JAI",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "RQ-KBL",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "5W-KIV",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "5W-KUT",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "5W-MED",
+        "Number of interviews": 15
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "PK-PEW",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "EY-PVG",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "W4-VIE",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-27",
+        "quota_id": "DP-VKO",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-11-28",
+        "quota_id": "EY-CPH",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-11-28",
+        "quota_id": "EY-LED",
+        "Number of interviews": 2
+    }
+]    
+
+ `;
