55946618abfef9ce367965c33c98c44adb739cb2
unknown <lb@silverliningresearch.com>
2025-03-29 20:58:11 +0000

    update new data

--- TIA_ASQ/interview_statistics.js
+++ TIA_ASQ/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-28",
         "quota_id": "OS-PRN",
         "Number of interviews": 1,
-        "download_time": "29-03-2025 21:42:47"
+        "download_time": "29-03-2025 21:57:26"
     },
     {
         "InterviewDate": "2024-01-28",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "29-03-2025 21:43:23"
+        "download_time": "29-03-2025 21:58:03"
     },
     {
         "InterviewDate": "2025-01-14",
