94fadb3a61ed788a329030ec6324ac2bff5fc34c
unknown <lb@silverliningresearch.com>
2025-03-29 15:33:18 +0000

    update new data

--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "29-03-2025 18:16:02"
+        "download_time": "29-03-2025 18:32:21"
     },
     {
         "InterviewDate": "2024-10-08",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "29-03-2025 16:16:09"
+        "download_time": "29-03-2025 16:32:28"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -1732,7 +1732,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-03-29",
         "InterviewerID": "33 - Udo Q.",
         "Q0B": "S\u00d8NDERVIG STRAND (FERIEHUS)",
-        "completes": 6
+        "completes": 7
     },
     {
         "InterviewDate": "2025-03-29",
