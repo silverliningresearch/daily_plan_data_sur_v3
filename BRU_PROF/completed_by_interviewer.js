53790c0c7f7a1468bb94d75ea39994de0cf4b5ee
unknown <lb@silverliningresearch.com>
2025-03-29 16:39:46 +0000

    update new data

--- BRU_PROF/completed_by_interviewer.js
+++ BRU_PROF/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "29-03-2025 17:25:14"
+        "download_time": "29-03-2025 17:39:30"
     },
     {
         "InterviewerID": 1,
--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,12 @@ let interview_statistics  = `[
         "InterviewDate": "2025-03-29",
         "quota_id": "adidas Originals\u2666TERMINAL 1\u2666Airside\u2666#02-73\u2666MP",
         "Number of interviews": 1,
-        "download_time": "30-03-2025 00:31:07"
+        "download_time": "30-03-2025 00:39:45"
+    },
+    {
+        "InterviewDate": "2025-03-30",
+        "quota_id": "Perfumes & Cosmetics (T1 ABE)\u2666TERMINAL 1\u2666Airside\u2666#01-09F\u2666PC",
+        "Number of interviews": 1
     }
 ]    
 
