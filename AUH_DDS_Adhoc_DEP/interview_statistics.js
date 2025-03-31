336447b280302483cafdb9cb0d79d6a1134973d0
unknown <lb@silverliningresearch.com>
2025-03-31 04:44:59 +0000

    update new data

--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-11-26",
         "quota_id": "6E-AMD",
         "Number of interviews": 1,
-        "download_time": "31-03-2025 08:22:02"
+        "download_time": "31-03-2025 08:44:58"
     },
     {
         "InterviewDate": "2024-11-26",
--- BRU_PROF/destAirlines_statistics.js
+++ BRU_PROF/destAirlines_statistics.js
@@ -3,7 +3,7 @@ let destAirlines_statistics  = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "EWR-UA",
         "Number of interviews": 2,
-        "download_time": "31-03-2025 06:12:51"
+        "download_time": "31-03-2025 06:38:46"
     },
     {
         "InterviewDate": "2023-04-01",
