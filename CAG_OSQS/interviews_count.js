bca984e9dffcebba9b23bca8b82052edae8c147a
unknown <lb@silverliningresearch.com>
2025-04-09 02:58:22 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "09-04-2025 10:45:57"
+        "download_time": "09-04-2025 10:56:40"
     },
     {
         "InterviewDate": "2024-12-28",
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "09-04-2025 05:48:04"
+        "download_time": "09-04-2025 05:58:06"
     },
     {
         "InterviewDate": "2024-10-08",
