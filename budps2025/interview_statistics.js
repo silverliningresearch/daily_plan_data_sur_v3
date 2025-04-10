cfbf14984b6aa01cbd1ba52938e20696471b31d8
unknown <lb@silverliningresearch.com>
2025-04-10 08:16:27 +0000

    update new data

--- budps2025/interview_statistics.js
+++ budps2025/interview_statistics.js
@@ -3,7 +3,67 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "CTA-FR",
         "Number of interviews": 1,
-        "download_time": "09-04-2025 13:26:16"
+        "download_time": "10-04-2025 10:16:26"
+    },
+    {
+        "InterviewDate": "2025-04-09",
+        "quota_id": "AMS-KL",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2025-04-09",
+        "quota_id": "ARN-FR",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-04-09",
+        "quota_id": "BGY-FR",
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2025-04-09",
+        "quota_id": "BVA-FR",
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2025-04-09",
+        "quota_id": "FRA-LH",
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2025-04-09",
+        "quota_id": "MUC-LH",
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2025-04-10",
+        "quota_id": "BRS-FR",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2025-04-10",
+        "quota_id": "FAO-FR",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-04-10",
+        "quota_id": "FCO-W6",
+        "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2025-04-10",
+        "quota_id": "MXP-W6",
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2025-04-10",
+        "quota_id": "VLC-FR",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-04-10",
+        "quota_id": "WAW-W6",
+        "Number of interviews": 7
     }
 ]    
 
