fe1874cf7c6f93fea128c3096b904b20203eed23
unknown <lb@silverliningresearch.com>
2025-04-12 15:32:07 +0000

    update new data

--- CAG_OSQS_MS/interviews_count.js
+++ CAG_OSQS_MS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2025-03-29",
         "quota_id": "adidas Originals\u2666TERMINAL 1\u2666Airside\u2666#02-73\u2666MP",
         "Number of interviews": 1,
-        "download_time": "12-04-2025 23:19:00"
+        "download_time": "12-04-2025 23:32:07"
     },
     {
         "InterviewDate": "2025-03-30",
