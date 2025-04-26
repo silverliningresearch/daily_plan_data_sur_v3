e06e8196583800bfc0693c1e5c42779607d05fb8
unknown <lb@silverliningresearch.com>
2025-04-26 04:30:31 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "26-04-2025 12:12:08"
+        "download_time": "26-04-2025 12:26:34"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -14883,7 +14883,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-04-26",
         "quota_id": "Aroma Truffle\u2666TERMINAL 1\u2666Airside\u2666#02-K19B\u2666CCD\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2025-04-26",
@@ -14932,6 +14932,11 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2025-04-26",
+        "quota_id": "Pedro\u2666TERMINAL 1\u2666Airside\u2666#02-74\u2666MP\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-04-26",
         "quota_id": "Prosegur T1 Arr Mtg Hall West\u2666TERMINAL 1\u2666Landside\u2666#01-K25\u2666MC\u2666Min8",
         "Number of interviews": 9
     },
@@ -14953,7 +14958,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-04-26",
         "quota_id": "Travelex T1 Arr Mtg Hall East\u2666TERMINAL 1\u2666Landside\u2666#01-K26\u2666MC\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 9
     },
     {
         "InterviewDate": "2025-04-26",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "26-04-2025 06:15:06"
+        "download_time": "26-04-2025 06:29:56"
     },
     {
         "InterviewDate": "2025-01-14",
