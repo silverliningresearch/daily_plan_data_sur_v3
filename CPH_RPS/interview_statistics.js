c537e24a37dbf5797ce8c74667c751748a0aca40
unknown <lb@silverliningresearch.com>
2025-04-30 08:56:52 +0000

    update new data

--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "CDG",
         "AirlineCode": "SK",
         "completed_interviews": 10,
-        "download_time": "30-04-2025 10:39:59"
+        "download_time": "30-04-2025 10:54:47"
     },
     {
         "Interview_Date": "2024-04-28",
@@ -22985,6 +22985,12 @@ let interview_statistics = `[
         "Dest": "YYZ",
         "AirlineCode": "SK",
         "completed_interviews": 16
+    },
+    {
+        "Interview_Date": "2025-04-30",
+        "Dest": "SIN",
+        "AirlineCode": "SQ",
+        "completed_interviews": 3
     }
 ]    
 
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "30-04-2025 10:43:34"
+        "download_time": "30-04-2025 10:56:32"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -3274,7 +3274,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-04-30",
         "InterviewerID": "8 - Sofie H.",
         "Q0B": "CROWNE PLAZA COPENHAGEN TOWERS",
-        "completes": 4
+        "completes": 5
     },
     {
         "InterviewDate": "2025-04-30",
