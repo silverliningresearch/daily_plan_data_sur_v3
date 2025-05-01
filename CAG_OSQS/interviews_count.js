cdc0c0d85fb5d668d695560992a31fd48d39d7dc
unknown <lb@silverliningresearch.com>
2025-05-01 13:30:10 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "01-05-2025 21:15:35"
+        "download_time": "01-05-2025 21:24:33"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -15733,7 +15733,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-05-01",
         "quota_id": "Gassan Watches (T2 DTS)\u2666TERMINAL 2\u2666Airside\u2666#02-208\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2025-05-01",
@@ -15743,7 +15743,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-05-01",
         "quota_id": "Hermes\u2666TERMINAL 2\u2666Airside\u2666#02-211\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2025-05-01",
@@ -15813,7 +15813,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-05-01",
         "quota_id": "Tory Burch\u2666TERMINAL 2\u2666Airside\u2666#02-213\u2666LX\u2666Min8",
-        "Number of interviews": 7
+        "Number of interviews": 8
     },
     {
         "InterviewDate": "2025-05-01",
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "01-05-2025 16:18:08"
+        "download_time": "01-05-2025 16:27:34"
     },
     {
         "InterviewDate": "2024-10-08",
