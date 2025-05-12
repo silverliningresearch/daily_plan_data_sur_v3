9073c86fbd71680e8e10c24768bb0b74306d7a66
unknown <lb@silverliningresearch.com>
2025-05-12 05:57:10 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "12-05-2025 13:28:22"
+        "download_time": "12-05-2025 13:54:49"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -17098,7 +17098,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-05-12",
         "quota_id": "Bacha Coffee\u2666TERMINAL 4\u2666Airside\u2666#02-31\u2666CCD\u2666Min8",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-05-12",
+        "quota_id": "KASHKA\u2666TERMINAL 4\u2666Airside\u2666#02-44\u2666MP\u2666Min8",
         "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-05-12",
+        "quota_id": "Perfumes & Cosmetics\u2666TERMINAL 4\u2666Airside\u2666#02-24\u2666PC\u2666Min25",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-05-12",
@@ -17108,7 +17118,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-05-12",
         "quota_id": "TWG Tea Boutique\u2666TERMINAL 4\u2666Airside\u2666#02-29\u2666CCD\u2666Min8",
-        "Number of interviews": 3
+        "Number of interviews": 5
     }
 ]    
 
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "12-05-2025 08:31:35"
+        "download_time": "12-05-2025 08:55:58"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -10753,7 +10753,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-05-12",
         "quota_id": "SV-RUH",
-        "Number of interviews": 3
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2025-05-12",
