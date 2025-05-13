73c52d96406159406facd9c86736abc27656eba7
unknown <lb@silverliningresearch.com>
2025-05-13 20:57:16 +0000

    update new data

--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-04-14",
         "quota_id": "PK-PEW",
         "Number of interviews": 7,
-        "download_time": "14-05-2025 00:33:03"
+        "download_time": "14-05-2025 00:57:14"
     },
     {
         "InterviewDate": "2025-04-15",
