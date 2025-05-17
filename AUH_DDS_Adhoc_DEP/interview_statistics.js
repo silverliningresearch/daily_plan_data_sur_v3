2128a544535e06563a0345abe15914f6020ff323
unknown <lb@silverliningresearch.com>
2025-05-17 21:27:05 +0000

    update new data

--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-04-14",
         "quota_id": "PK-PEW",
         "Number of interviews": 7,
-        "download_time": "18-05-2025 00:56:57"
+        "download_time": "18-05-2025 01:27:04"
     },
     {
         "InterviewDate": "2025-04-15",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "17-05-2025 22:56:20"
+        "download_time": "17-05-2025 23:26:27"
     },
     {
         "InterviewDate": "2025-01-14",
