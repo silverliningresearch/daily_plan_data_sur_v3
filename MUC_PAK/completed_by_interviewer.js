cb2151b48afcd576864f4d5840f4024fac90b9fc
unknown <lb@silverliningresearch.com>
2025-05-23 05:38:29 +0000

    update new data

---
+++ MUC_PAK/completed_by_interviewer.js
@@ -0,0 +1,135 @@
+let completed_by_interviewer = `[
+    {
+        "InterviewerID": "20 \u2013 Maria",
+        "Interview_Date": "2024-09-14",
+        "completed_interviews": 17,
+        "download_time": "23-05-2025 07:38:28"
+    },
+    {
+        "InterviewerID": "20 \u2013 Maria",
+        "Interview_Date": "2024-09-17",
+        "completed_interviews": 30
+    },
+    {
+        "InterviewerID": "20 \u2013 Maria",
+        "Interview_Date": "2024-09-18",
+        "completed_interviews": 18
+    },
+    {
+        "InterviewerID": "20 \u2013 Maria",
+        "Interview_Date": "2024-09-20",
+        "completed_interviews": 30
+    },
+    {
+        "InterviewerID": "20 \u2013 Maria",
+        "Interview_Date": "2024-09-23",
+        "completed_interviews": 15
+    },
+    {
+        "InterviewerID": "20 \u2013 Maria",
+        "Interview_Date": "2024-10-08",
+        "completed_interviews": 15
+    },
+    {
+        "InterviewerID": "20 \u2013 Maria",
+        "Interview_Date": "2024-10-09",
+        "completed_interviews": 27
+    },
+    {
+        "InterviewerID": "20 \u2013 Maria",
+        "Interview_Date": "2024-10-11",
+        "completed_interviews": 9
+    },
+    {
+        "InterviewerID": "29 \u2013 Werner",
+        "Interview_Date": "2024-09-04",
+        "completed_interviews": 19
+    },
+    {
+        "InterviewerID": "29 \u2013 Werner",
+        "Interview_Date": "2024-09-06",
+        "completed_interviews": 11
+    },
+    {
+        "InterviewerID": "29 \u2013 Werner",
+        "Interview_Date": "2024-09-09",
+        "completed_interviews": 28
+    },
+    {
+        "InterviewerID": "29 \u2013 Werner",
+        "Interview_Date": "2024-09-30",
+        "completed_interviews": 16
+    },
+    {
+        "InterviewerID": "29 \u2013 Werner",
+        "Interview_Date": "2024-10-08",
+        "completed_interviews": 15
+    },
+    {
+        "InterviewerID": "29 \u2013 Werner",
+        "Interview_Date": "2024-10-10",
+        "completed_interviews": 17
+    },
+    {
+        "InterviewerID": "29 \u2013 Werner",
+        "Interview_Date": "2024-10-14",
+        "completed_interviews": 23
+    },
+    {
+        "InterviewerID": "34 \u2013 Maja",
+        "Interview_Date": "2024-09-03",
+        "completed_interviews": 15
+    },
+    {
+        "InterviewerID": "34 \u2013 Maja",
+        "Interview_Date": "2024-09-04",
+        "completed_interviews": 14
+    },
+    {
+        "InterviewerID": "34 \u2013 Maja",
+        "Interview_Date": "2024-09-05",
+        "completed_interviews": 18
+    },
+    {
+        "InterviewerID": "36 \u2013 Stefan",
+        "Interview_Date": "2024-09-03",
+        "completed_interviews": 9
+    },
+    {
+        "InterviewerID": "36 \u2013 Stefan",
+        "Interview_Date": "2024-09-05",
+        "completed_interviews": 26
+    },
+    {
+        "InterviewerID": "36 \u2013 Stefan",
+        "Interview_Date": "2024-09-08",
+        "completed_interviews": 25
+    },
+    {
+        "InterviewerID": "36 \u2013 Stefan",
+        "Interview_Date": "2024-09-15",
+        "completed_interviews": 30
+    },
+    {
+        "InterviewerID": "36 \u2013 Stefan",
+        "Interview_Date": "2024-09-21",
+        "completed_interviews": 25
+    },
+    {
+        "InterviewerID": "36 \u2013 Stefan",
+        "Interview_Date": "2024-10-05",
+        "completed_interviews": 19
+    },
+    {
+        "InterviewerID": "36 \u2013 Stefan",
+        "Interview_Date": "2024-10-06",
+        "completed_interviews": 10
+    },
+    {
+        "InterviewerID": "36 \u2013 Stefan",
+        "Interview_Date": "2024-10-16",
+        "completed_interviews": 20
+    }
+]    
+
+ `;
---
+++ MUC_PAK/interview_statistics.js
@@ -0,0 +1,453 @@
+let interview_statistics = `[
+    {
+        "InterviewDate": "2024-09-03",
+        "InterviewerID": "34 \u2013 Maja",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 5,
+        "download_time": "23-05-2025 07:38:28"
+    },
+    {
+        "InterviewDate": "2024-09-03",
+        "InterviewerID": "34 \u2013 Maja",
+        "quota_id": "8-8",
+        "Location": 8,
+        "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-09-03",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "11-11",
+        "Location": 11,
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-09-04",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-09-04",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "11-11",
+        "Location": 11,
+        "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-09-04",
+        "InterviewerID": "34 \u2013 Maja",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-09-04",
+        "InterviewerID": "34 \u2013 Maja",
+        "quota_id": "17-17",
+        "Location": 17,
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-09-04",
+        "InterviewerID": "34 \u2013 Maja",
+        "quota_id": "18-18",
+        "Location": 18,
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-09-05",
+        "InterviewerID": "34 \u2013 Maja",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-09-05",
+        "InterviewerID": "34 \u2013 Maja",
+        "quota_id": "8-8",
+        "Location": 8,
+        "Number of interviews": 14
+    },
+    {
+        "InterviewDate": "2024-09-05",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-09-05",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 18
+    },
+    {
+        "InterviewDate": "2024-09-06",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "21-21",
+        "Location": 21,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-09-06",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "22-22",
+        "Location": 22,
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-09-08",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-09-08",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "18-18",
+        "Location": 18,
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-09-08",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "21-21",
+        "Location": 21,
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-09-08",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "22-22",
+        "Location": 22,
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-09-09",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 16
+    },
+    {
+        "InterviewDate": "2024-09-09",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "17-17",
+        "Location": 17,
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-09-09",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "18-18",
+        "Location": 18,
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-09-09",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "22-22",
+        "Location": 22,
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-14",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2024-09-14",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "17-17",
+        "Location": 17,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-09-14",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "18-18",
+        "Location": 18,
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2024-09-15",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 19
+    },
+    {
+        "InterviewDate": "2024-09-15",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 11
+    },
+    {
+        "InterviewDate": "2024-09-17",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 18
+    },
+    {
+        "InterviewDate": "2024-09-17",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-09-17",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "8-8",
+        "Location": 8,
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-09-18",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-09-18",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "21-21",
+        "Location": 21,
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-09-18",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "22-22",
+        "Location": 22,
+        "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-09-20",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 14
+    },
+    {
+        "InterviewDate": "2024-09-20",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 16
+    },
+    {
+        "InterviewDate": "2024-09-21",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2024-09-21",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "21-21",
+        "Location": 21,
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-09-21",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "22-22",
+        "Location": 22,
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-09-23",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-09-23",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "11-11",
+        "Location": 11,
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-09-30",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-09-30",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "8-8",
+        "Location": 8,
+        "Number of interviews": 13
+    },
+    {
+        "InterviewDate": "2024-10-05",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-10-05",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "17-17",
+        "Location": 17,
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-10-05",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "18-18",
+        "Location": 18,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-10-06",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-10-06",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "11-11",
+        "Location": 11,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-10-08",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2024-10-08",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "17-17",
+        "Location": 17,
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2024-10-08",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "18-18",
+        "Location": 18,
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-10-08",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-10-08",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "11-11",
+        "Location": 11,
+        "Number of interviews": 11
+    },
+    {
+        "InterviewDate": "2024-10-09",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 15
+    },
+    {
+        "InterviewDate": "2024-10-09",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "8-8",
+        "Location": 8,
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2024-10-10",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "11-11",
+        "Location": 11,
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2024-10-10",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "17-17",
+        "Location": 17,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2024-10-10",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "18-18",
+        "Location": 18,
+        "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2024-10-11",
+        "InterviewerID": "20 \u2013 Maria",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 9
+    },
+    {
+        "InterviewDate": "2024-10-14",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 15
+    },
+    {
+        "InterviewDate": "2024-10-14",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "11-11",
+        "Location": 11,
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-10-14",
+        "InterviewerID": "29 \u2013 Werner",
+        "quota_id": "18-18",
+        "Location": 18,
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2024-10-16",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "2-2",
+        "Location": 2,
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2024-10-16",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "5-5",
+        "Location": 5,
+        "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2024-10-16",
+        "InterviewerID": "36 \u2013 Stefan",
+        "quota_id": "6-6",
+        "Location": 6,
+        "Number of interviews": 5
+    }
+]    
+
+ `;
