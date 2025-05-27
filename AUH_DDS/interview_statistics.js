41a45008380d225bc7c916950023430781990bb2
unknown <lb@silverliningresearch.com>
2025-05-27 22:55:26 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "28-05-2025 02:29:39"
+        "download_time": "28-05-2025 02:54:42"
     },
     {
         "InterviewDate": "2024-04-13",
--- AUH_DDS_Adhoc_DEP/interview_statistics.js
+++ AUH_DDS_Adhoc_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-04-14",
         "quota_id": "PK-PEW",
         "Number of interviews": 7,
-        "download_time": "28-05-2025 02:31:33"
+        "download_time": "28-05-2025 02:55:24"
     },
     {
         "InterviewDate": "2025-04-15",
