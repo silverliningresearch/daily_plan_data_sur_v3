25cb713d2172c3d1fcff6d6228acca386e29e899
unknown <lb@silverliningresearch.com>
2025-06-15 05:26:42 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "15-06-2025 12:54:28"
+        "download_time": "15-06-2025 13:24:27"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -22197,13 +22197,18 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2025-06-15",
+        "quota_id": "Famous Amos\u2666TERMINAL 3\u2666Landside\u2666#02-97\u2666LS\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2025-06-15",
         "quota_id": "Guardian\u2666TERMINAL 2\u2666Landside\u2666#02-13\u2666LS\u2666Min8",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-06-15",
         "quota_id": "LEVURE NATURELLE\u2666TERMINAL 2\u2666Landside\u2666#02-22\u2666LS\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-06-15",
@@ -22288,7 +22293,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-06-15",
         "quota_id": "WH Smith (T2 Landside)\u2666TERMINAL 2\u2666Landside\u2666#02-23\u2666OW\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-06-15",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "15-06-2025 07:55:39"
+        "download_time": "15-06-2025 08:25:38"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -12492,7 +12492,17 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2025-06-15",
+        "quota_id": "SV-DMM",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-06-15",
         "quota_id": "XY-DMM",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-06-15",
+        "quota_id": "SV-GIZ",
         "Number of interviews": 1
     },
     {
@@ -12513,7 +12523,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-06-15",
         "quota_id": "SV-RUH",
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2025-06-15",
