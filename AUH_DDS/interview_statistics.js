d922ed4141d7d0a50db3d58019642553f79c3478
unknown <lb@silverliningresearch.com>
2025-06-25 23:25:34 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "26-06-2025 02:56:44"
+        "download_time": "26-06-2025 03:24:51"
     },
     {
         "InterviewDate": "2024-04-13",
--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "26-06-2025 06:55:31"
+        "download_time": "26-06-2025 07:23:59"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -23514,6 +23514,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-06-25",
         "quota_id": "Sprint-Cass (T3 DTN)\u2666TERMINAL 3\u2666Airside\u2666#02-61\u2666EL\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2025-06-26",
+        "quota_id": "Hugo Boss\u2666TERMINAL 2\u2666Airside\u2666#02-232\u2666LX\u2666Min8",
+        "Number of interviews": 4
     }
 ]    
 
