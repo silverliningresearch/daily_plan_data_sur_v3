ed1f3ddcd8458565fda6090d6da86f0977b3f664
unknown <lb@silverliningresearch.com>
2025-06-27 00:32:31 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "27-06-2025 07:53:57"
+        "download_time": "27-06-2025 08:26:28"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -23587,8 +23587,13 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "2025-06-27",
+        "quota_id": "LACOSTE\u2666TERMINAL 1\u2666Airside\u2666#02-30F\u2666MP\u2666Min8",
+        "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "2025-06-27",
         "quota_id": "Oakley\u2666TERMINAL 2\u2666Airside\u2666#02-244\u2666MP\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2025-06-27",
@@ -23598,17 +23603,17 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-06-27",
         "quota_id": "The Cocoa Trees (T1 DTW)\u2666TERMINAL 1\u2666Airside\u2666#02-35N\u2666CCD\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2025-06-27",
         "quota_id": "Tommy Hilfiger\u2666TERMINAL 2\u2666Airside\u2666#026-055-01A\u2666MP\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-06-27",
         "quota_id": "iStudio\u2666TERMINAL 2\u2666Airside\u2666#02-238\u2666EL\u2666Min8",
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "27-06-2025 02:55:01"
+        "download_time": "27-06-2025 03:29:14"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -13132,13 +13132,18 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2025-06-27",
+        "quota_id": "XY-AHB",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-06-27",
         "quota_id": "SV-CMN",
         "Number of interviews": 1
     },
     {
         "InterviewDate": "2025-06-27",
         "quota_id": "SV-GIZ",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-06-27",
