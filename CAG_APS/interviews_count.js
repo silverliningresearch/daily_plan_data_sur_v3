7de1afc10a06c59292fce0c4ef57d6978c0840cd
unknown <lb@silverliningresearch.com>
2025-07-04 01:35:28 +0000

    update new data

--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T3_GA_CGK",
         "interviewerID": "02",
         "Number of interviews": 7,
-        "download_time": "04-07-2025 08:55:10"
+        "download_time": "04-07-2025 09:28:25"
     },
     {
         "InterviewDate": "01-06-2025",
@@ -806,6 +806,12 @@ let interview_statistics  = `[
     },
     {
         "InterviewDate": "04-07-2025",
+        "quota_id": "T1_EK_DXB",
+        "interviewerID": "28",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "04-07-2025",
         "quota_id": "T1_TR_CNX",
         "interviewerID": "28",
         "Number of interviews": 3
@@ -814,7 +820,7 @@ let interview_statistics  = `[
         "InterviewDate": "04-07-2025",
         "quota_id": "T1_TR_KUL",
         "interviewerID": "28",
-        "Number of interviews": 7
+        "Number of interviews": 10
     },
     {
         "InterviewDate": "04-07-2025",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "04-07-2025 03:55:44"
+        "download_time": "04-07-2025 04:29:40"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -13537,6 +13537,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2025-07-04",
+        "quota_id": "SV-AHB",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-07-04",
         "quota_id": "XY-DMM",
         "Number of interviews": 2
     },
@@ -13548,6 +13553,11 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-07-04",
         "quota_id": "SV-RUH",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-07-04",
+        "quota_id": "XY-RUH",
         "Number of interviews": 1
     }
 ]    
