811fb00afbecb76f1c98e427d6cd0f2af3c1b8b4
unknown <lb@silverliningresearch.com>
2025-07-09 04:32:02 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "09-07-2025 11:55:26"
+        "download_time": "09-07-2025 12:28:21"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -25053,6 +25053,11 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-07-09",
         "quota_id": "Discover Singapore (Premises F) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03A\u2666SV\u2666Min8",
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2025-07-09",
+        "quota_id": "Furla\u2666TERMINAL 3\u2666Airside\u2666#02-14\u2666MP\u2666Min8",
         "Number of interviews": 2
     },
     {
@@ -25078,7 +25083,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-07-09",
         "quota_id": "Longchamp\u2666TERMINAL 3\u2666Airside\u2666#02-04\u2666MP\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-07-09",
@@ -25088,7 +25093,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-07-09",
         "quota_id": "Perfumes & Cosmetics (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-03\u2666PC\u2666Min15",
-        "Number of interviews": 2
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-07-09",
@@ -25098,7 +25103,7 @@ let interview_statistics  = `[
     {
         "InterviewDate": "2025-07-09",
         "quota_id": "Roger Vivier\u2666TERMINAL 1\u2666Airside\u2666#02-13AF\u2666LX\u2666Min8",
-        "Number of interviews": 1
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-07-09",
@@ -25119,6 +25124,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-07-09",
         "quota_id": "WH Smith - Premises H (Air) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-07\u2666OW\u2666Min8",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-07-09",
+        "quota_id": "iStudio\u2666TERMINAL 1\u2666Airside\u2666#02-13F\u2666EL\u2666Min8",
+        "Number of interviews": 3
     }
 ]    
 
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "09-07-2025 06:56:52"
+        "download_time": "09-07-2025 07:30:57"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -13838,7 +13838,12 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-07-09",
         "quota_id": "BA-LHR",
-        "Number of interviews": 1
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-07-09",
+        "quota_id": "SV-MAN",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-07-09",
