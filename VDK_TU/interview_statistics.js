65f6e1906e0133e98fd8703286ebeed51e9caa2e
unknown <lb@silverliningresearch.com>
2025-07-27 19:59:51 +0000

    update new data

--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "27-07-2025 21:29:18"
+        "download_time": "27-07-2025 21:59:50"
     },
     {
         "InterviewDate": "2025-01-14",
