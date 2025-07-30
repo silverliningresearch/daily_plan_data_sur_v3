afc509575d32c054aa4fa72891ea2642d97895ea
unknown <lb@silverliningresearch.com>
2025-07-30 00:59:41 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "30-07-2025 08:28:34"
+        "download_time": "30-07-2025 08:57:02"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -28059,6 +28059,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-07-29",
         "quota_id": "Singapore Food Street\u2666TERMINAL 3\u2666Airside\u2666#03-011\u2666FBA\u2666Min25",
         "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-07-30",
+        "quota_id": "Adidas\u2666TERMINAL 4\u2666Airside\u2666#02-32\u2666MP\u2666Min8",
+        "Number of interviews": 3
     },
     {
         "InterviewDate": "2025-07-30",
--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "EY-AUH",
         "InterviewerID": "TEST",
         "Number of interviews": 1,
-        "download_time": "30-07-2025 04:29:56"
+        "download_time": "30-07-2025 04:57:52"
     },
     {
         "InterviewDate": "2025-07-22",
