d4b527f29395de41ee4fade875357b8e6df22e94
unknown <lb@silverliningresearch.com>
2025-08-12 23:25:20 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "13-08-2025 06:54:06"
+        "download_time": "13-08-2025 07:23:48"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -30009,6 +30009,11 @@ let interview_statistics  = `[
         "InterviewDate": "2025-08-12",
         "quota_id": "Travelex T2 DT South\u2666TERMINAL 2\u2666Airside\u2666#02-209A\u2666MC\u2666Min8",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2025-08-13",
+        "quota_id": "The Coffee Bean & Tea Leaf\u2666TERMINAL 2\u2666Airside\u2666#02-800\u2666FBA\u2666Min8",
+        "Number of interviews": 2
     }
 ]    
 
