169a99ab3ab97282bb3e5aa8c340e73dddad41e8
unknown <lb@silverliningresearch.com>
2025-08-13 22:55:30 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "14-08-2025 00:25:00"
+        "download_time": "14-08-2025 00:53:07"
     },
     {
         "InterviewDate": "2023-04-01",
--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "TPE",
         "AirlineCode": "SQ",
         "completed_interviews": 1,
-        "download_time": "14-08-2025 06:25:42"
+        "download_time": "14-08-2025 06:53:26"
     },
     {
         "Interview_Date": "2025-01-01",
@@ -22175,6 +22175,18 @@ let interview_statistics = `[
         "Dest": "WUH",
         "AirlineCode": "TR",
         "completed_interviews": 4
+    },
+    {
+        "Interview_Date": "2025-08-14",
+        "Dest": "BOM",
+        "AirlineCode": "SQ",
+        "completed_interviews": 3
+    },
+    {
+        "Interview_Date": "2025-08-14",
+        "Dest": "CGK",
+        "AirlineCode": "SQ",
+        "completed_interviews": 1
     },
     {
         "Interview_Date": "2025-08-14",
