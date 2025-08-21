f49634b0f26f95164c9bd714ee451395a65e96d9
unknown <lb@silverliningresearch.com>
2025-08-21 21:25:55 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "EY-AUH",
         "InterviewerID": "TEST",
         "Number of interviews": 1,
-        "download_time": "22-08-2025 00:55:05"
+        "download_time": "22-08-2025 01:25:07"
     },
     {
         "InterviewDate": "2025-07-22",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "21-08-2025 22:55:52"
+        "download_time": "21-08-2025 23:25:54"
     },
     {
         "InterviewDate": "2025-01-14",
