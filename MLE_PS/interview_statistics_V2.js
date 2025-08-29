81aef76dbe7c68eb590eddfb7a506731c2c2fd80
unknown <lb@silverliningresearch.com>
2025-08-29 02:57:13 +0000

    update new data

--- MLE_PS/interview_statistics_V2.js
+++ MLE_PS/interview_statistics_V2.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "quota_id": "EY-AUH",
         "InterviewerID": "TEST",
         "Number of interviews": 1,
-        "download_time": "29-08-2025 06:30:30"
+        "download_time": "29-08-2025 06:56:00"
     },
     {
         "InterviewDate": "2025-07-22",
