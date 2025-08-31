b7f3a422a1bb06d3e9bd246e8664d253e4e13c41
unknown <lb@silverliningresearch.com>
2025-08-31 01:36:43 +0000

    update new data

--- CAG_OSQS/interviews_count.js
+++ CAG_OSQS/interviews_count.js
@@ -3,7 +3,7 @@ let interview_statistics  = `[
         "InterviewDate": "2024-12-28",
         "quota_id": "A-Roy Thai Restaurant\u2666TERMINAL 2\u2666Landside\u2666#03-14\u2666FBL\u2666Min8",
         "Number of interviews": 8,
-        "download_time": "31-08-2025 08:55:31"
+        "download_time": "31-08-2025 09:32:03"
     },
     {
         "InterviewDate": "2024-12-28",
@@ -32559,6 +32559,21 @@ let interview_statistics  = `[
         "InterviewDate": "2025-08-30",
         "quota_id": "Time Emporium\u2666TERMINAL 4\u2666Airside\u2666#02-27\u2666LX\u2666Min8",
         "Number of interviews": 10
+    },
+    {
+        "InterviewDate": "2025-08-31",
+        "quota_id": "Bacha Coffee (Duplex) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-10A\u2666CCD\u2666Min8",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-08-31",
+        "quota_id": "LT T3 DT South\u2666TERMINAL 3\u2666Airside\u2666#02-10/11/12\u2666LT\u2666Min15",
+        "Number of interviews": 2
+    },
+    {
+        "InterviewDate": "2025-08-31",
+        "quota_id": "WH Smith - Premises H (Air) (T3 DTS)\u2666TERMINAL 3\u2666Airside\u2666#02-07\u2666OW\u2666Min8",
+        "Number of interviews": 1
     }
 ]    
 
--- JED_BHT/interview_statistics.js
+++ JED_BHT/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-05-29",
         "quota_id": "XY-KZN",
         "Number of interviews": 1,
-        "download_time": "31-08-2025 03:57:20"
+        "download_time": "31-08-2025 04:36:12"
     },
     {
         "InterviewDate": "2025-06-25",
