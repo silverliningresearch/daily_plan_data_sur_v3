1af02f338f535fa77649b4e04ea755fa6672c4ca
unknown <lb@silverliningresearch.com>
2025-09-11 00:54:04 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "TPE",
         "AirlineCode": "SQ",
         "completed_interviews": 1,
-        "download_time": "11-09-2025 08:23:48"
+        "download_time": "11-09-2025 08:53:09"
     },
     {
         "Interview_Date": "2025-01-01",
@@ -24759,6 +24759,12 @@ let interview_statistics = `[
     {
         "Interview_Date": "2025-09-11",
         "Dest": "CGK",
+        "AirlineCode": "SQ",
+        "completed_interviews": 4
+    },
+    {
+        "Interview_Date": "2025-09-11",
+        "Dest": "CGK",
         "AirlineCode": "TR",
         "completed_interviews": 3
     },
@@ -24797,6 +24803,12 @@ let interview_statistics = `[
         "Dest": "KIX",
         "AirlineCode": "SQ",
         "completed_interviews": 8
+    },
+    {
+        "Interview_Date": "2025-09-11",
+        "Dest": "NRT",
+        "AirlineCode": "TR",
+        "completed_interviews": 4
     },
     {
         "Interview_Date": "2025-09-11",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "11-09-2025 02:26:37"
+        "download_time": "11-09-2025 02:53:58"
     },
     {
         "InterviewDate": "2025-01-14",
