16f312a2aebecef34b54bdb9ca4216be5453badb
unknown <lb@silverliningresearch.com>
2025-09-12 19:55:12 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "12-09-2025 23:24:18"
+        "download_time": "12-09-2025 23:54:30"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -29734,7 +29734,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 1,
-        "Number of interviews": 4
+        "Number of interviews": 5
     },
     {
         "InterviewDate": "2025-09-12",
@@ -29776,7 +29776,7 @@ let interview_statistics = `[
         "quota_id": "7-7",
         "Survey": 7,
         "Location": 8888888,
-        "Number of interviews": 2
+        "Number of interviews": 3
     }
 ]    
 
--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "12-09-2025 22:24:37"
+        "download_time": "12-09-2025 22:54:51"
     },
     {
         "InterviewDate": "2024-10-08",
@@ -20672,6 +20672,11 @@ let interview_statistics = `[
     },
     {
         "InterviewDate": "2025-09-12",
+        "quota_id": "XY-CAI",
+        "Number of interviews": 1
+    },
+    {
+        "InterviewDate": "2025-09-12",
         "quota_id": "F3-DMM",
         "Number of interviews": 3
     },
@@ -20743,7 +20748,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2025-09-12",
         "quota_id": "XY-RUH",
-        "Number of interviews": 9
+        "Number of interviews": 11
     },
     {
         "InterviewDate": "2025-09-12",
