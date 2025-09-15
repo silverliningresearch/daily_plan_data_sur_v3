e82af91d5da9df0e75468d7aaab46f81ceaba254
unknown <lb@silverliningresearch.com>
2025-09-15 21:55:23 +0000

    update new data

--- MUC_PAK/interview_statistics.js
+++ MUC_PAK/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "quota_id": "2-2",
         "Location": 2,
         "Number of interviews": 8,
-        "download_time": "15-09-2025 23:24:50"
+        "download_time": "15-09-2025 23:54:47"
     },
     {
         "InterviewDate": "2025-06-04",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "15-09-2025 23:25:20"
+        "download_time": "15-09-2025 23:55:16"
     },
     {
         "InterviewDate": "2025-01-14",
