887c2807ab4967ce4f808b5aaa33aeb41de396c3
unknown <lb@silverliningresearch.com>
2025-09-16 14:30:50 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "TPE",
         "AirlineCode": "SQ",
         "completed_interviews": 1,
-        "download_time": "16-09-2025 21:53:20"
+        "download_time": "16-09-2025 22:24:16"
     },
     {
         "Interview_Date": "2025-01-01",
@@ -25367,6 +25367,12 @@ let interview_statistics = `[
         "Dest": "HND",
         "AirlineCode": "NH",
         "completed_interviews": 5
+    },
+    {
+        "Interview_Date": "2025-09-16",
+        "Dest": "ICN",
+        "AirlineCode": "TW",
+        "completed_interviews": 7
     },
     {
         "Interview_Date": "2025-09-16",
--- JED_DEP/interview_statistics.js
+++ JED_DEP/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-01",
         "quota_id": "SV-AHB",
         "Number of interviews": 1,
-        "download_time": "16-09-2025 16:54:44"
+        "download_time": "16-09-2025 17:28:23"
     },
     {
         "InterviewDate": "2024-10-01",
@@ -17739,6 +17739,11 @@ let interview_statistics = `[
         "InterviewDate": "2025-09-16",
         "quota_id": "F3-AHB",
         "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2025-09-16",
+        "quota_id": "EY-AUH",
+        "Number of interviews": 2
     },
     {
         "InterviewDate": "2025-09-16",
