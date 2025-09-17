3c8d75d44058c4ae77fe400ec4564b2f4d94e911
unknown <lb@silverliningresearch.com>
2025-09-17 21:00:16 +0000

    update new data

--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AUH",
         "AirlineCode": "EY",
         "completed_interviews": 1,
-        "download_time": "17-09-2025 22:25:15"
+        "download_time": "17-09-2025 22:54:44"
     },
     {
         "Interview_Date": "2024-05-31",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "17-09-2025 22:30:26"
+        "download_time": "17-09-2025 22:59:28"
     },
     {
         "InterviewDate": "2025-01-14",
