0b0c1d4723c7c0294d21e17a21ceea04219ced96
unknown <lb@silverliningresearch.com>
2025-09-23 14:32:36 +0000

    update new data

--- BUD_Commercial/interview_statistics.js
+++ BUD_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "BCN-FR",
         "Number of interviews": 3,
-        "download_time": "23-09-2025 15:53:44"
+        "download_time": "23-09-2025 16:24:06"
     },
     {
         "InterviewDate": "2024-01-02",
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "23-09-2025 15:57:03"
+        "download_time": "23-09-2025 16:30:59"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -11710,7 +11710,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-09-23",
         "InterviewerID": "35 - Morten R",
         "Q0B": "CITYCAMP ASSENS STRAND",
-        "completes": 7
+        "completes": 8
     },
     {
         "InterviewDate": "2025-09-23",
