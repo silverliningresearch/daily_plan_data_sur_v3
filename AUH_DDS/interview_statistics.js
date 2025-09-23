8969d62cb07a39d51afa89024a956faaca95b44b
unknown <lb@silverliningresearch.com>
2025-09-23 13:57:06 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "23-09-2025 17:25:50"
+        "download_time": "23-09-2025 17:55:51"
     },
     {
         "InterviewDate": "2024-04-13",
@@ -30434,7 +30434,7 @@ let interview_statistics = `[
         "quota_id": "3-3",
         "Survey": 3,
         "Location": 1,
-        "Number of interviews": 5
+        "Number of interviews": 7
     },
     {
         "InterviewDate": "2025-09-23",
@@ -30469,7 +30469,7 @@ let interview_statistics = `[
         "quota_id": "7-7",
         "Survey": 7,
         "Location": 8888888,
-        "Number of interviews": 6
+        "Number of interviews": 7
     }
 ]    
 
--- VDK_TU/interview_statistics.js
+++ VDK_TU/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "InterviewerID": "12 - Jon H.",
         "Q0B": "COMWELL AARHUS",
         "completes": 5,
-        "download_time": "23-09-2025 15:27:06"
+        "download_time": "23-09-2025 15:57:03"
     },
     {
         "InterviewDate": "2025-01-14",
@@ -11698,7 +11698,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-09-23",
         "InterviewerID": "33 - Udo Q.",
         "Q0B": "BJERREGAARD STRAND (FERIEHUS)",
-        "completes": 11
+        "completes": 12
     },
     {
         "InterviewDate": "2025-09-23",
@@ -11710,7 +11710,7 @@ let interview_statistics = `[
         "InterviewDate": "2025-09-23",
         "InterviewerID": "35 - Morten R",
         "Q0B": "CITYCAMP ASSENS STRAND",
-        "completes": 6
+        "completes": 7
     },
     {
         "InterviewDate": "2025-09-23",
