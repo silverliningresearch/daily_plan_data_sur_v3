5d2933b0971e520ec8a1473d650980aec0300f8d
unknown <lb@silverliningresearch.com>
2025-09-27 14:57:41 +0000

    update new data

--- BER_PS_2024/interview_statistics.js
+++ BER_PS_2024/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-03",
         "quota_id": "AMS - KLM Royal Dutch Airlines",
         "Number of interviews": 2,
-        "download_time": "27-09-2025 16:27:39"
+        "download_time": "27-09-2025 16:54:15"
     },
     {
         "InterviewDate": "2024-01-03",
@@ -20734,6 +20734,11 @@ let interview_statistics = `[
         "InterviewDate": "2025-09-27",
         "quota_id": "LCA - easyJet Europe",
         "Number of interviews": 6
+    },
+    {
+        "InterviewDate": "2025-09-27",
+        "quota_id": "LIS - TAP Air Portugal",
+        "Number of interviews": 5
     }
 ]    
 
--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AUH",
         "AirlineCode": "EY",
         "completed_interviews": 1,
-        "download_time": "27-09-2025 16:27:56"
+        "download_time": "27-09-2025 16:54:31"
     },
     {
         "Interview_Date": "2024-05-31",
@@ -29711,6 +29711,12 @@ let interview_statistics = `[
         "Dest": "CDG",
         "AirlineCode": "AF",
         "completed_interviews": 21
+    },
+    {
+        "Interview_Date": "2025-09-27",
+        "Dest": "CDG",
+        "AirlineCode": "SK",
+        "completed_interviews": 3
     },
     {
         "Interview_Date": "2025-09-27",
