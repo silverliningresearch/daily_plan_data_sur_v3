20129c246ca1487e7916cf06f3afa285d2d8ac0c
unknown <lb@silverliningresearch.com>
2025-09-29 23:27:09 +0000

    update new data

--- BRU_PROF/interview_statistics.js
+++ BRU_PROF/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2023-04-01",
         "quota_id": "HU492-PEK",
         "Number of interviews": 7,
-        "download_time": "30-09-2025 00:53:40"
+        "download_time": "30-09-2025 01:23:38"
     },
     {
         "InterviewDate": "2023-04-01",
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2025-06-04",
         "completed_interviews": 20,
-        "download_time": "30-09-2025 00:56:02"
+        "download_time": "30-09-2025 01:25:58"
     },
     {
         "InterviewerID": "20 \u2013 Maria",
