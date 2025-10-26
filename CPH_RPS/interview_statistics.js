3fc1ec5887b652ba316214b31a54f6bdda06c3bf
unknown <lb@silverliningresearch.com>
2025-10-26 03:25:24 +0000

    update new data

--- CPH_RPS/interview_statistics.js
+++ CPH_RPS/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AUH",
         "AirlineCode": "EY",
         "completed_interviews": 1,
-        "download_time": "26-10-2025 03:55:06"
+        "download_time": "26-10-2025 04:23:43"
     },
     {
         "Interview_Date": "2024-05-31",
--- MUC_PAK/completed_by_interviewer.js
+++ MUC_PAK/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "20 \u2013 Maria",
         "Interview_Date": "2025-06-04",
         "completed_interviews": 20,
-        "download_time": "26-10-2025 03:56:30"
+        "download_time": "26-10-2025 04:24:47"
     },
     {
         "InterviewerID": "20 \u2013 Maria",
