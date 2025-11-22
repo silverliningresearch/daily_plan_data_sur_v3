f6d8cea64aab72ed4d3f3a9d06cc771c5cb3a5e7
unknown <lb@silverliningresearch.com>
2025-11-22 23:30:40 +0000

    update new data

--- BUD_Commercial/interview_statistics.js
+++ BUD_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "BCN-FR",
         "Number of interviews": 3,
-        "download_time": "22-11-2025 23:55:24"
+        "download_time": "23-11-2025 00:25:04"
     },
     {
         "InterviewDate": "2024-01-02",
--- CAG_APS/interviews_count.js
+++ CAG_APS/interviews_count.js
@@ -4,7 +4,7 @@ let interview_statistics  = `[
         "quota_id": "T1_CZ_CAN",
         "interviewerID": "02",
         "Number of interviews": 7,
-        "download_time": "23-11-2025 06:58:29"
+        "download_time": "23-11-2025 07:27:53"
     },
     {
         "InterviewDate": "01-11-2025",
@@ -1835,6 +1835,12 @@ let interview_statistics  = `[
         "quota_id": "T1_TR_KMG",
         "interviewerID": "29",
         "Number of interviews": 3
+    },
+    {
+        "InterviewDate": "23-11-2025",
+        "quota_id": "T1_JL_NRT",
+        "interviewerID": "04",
+        "Number of interviews": 4
     }
 ]    
 
