b4abe717b90459ff3f824905162dff613c2ab930
unknown <lb@silverliningresearch.com>
2026-05-20 21:27:00 +0000

    update new data

--- BUD_Route/interview_statistics.js
+++ BUD_Route/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "LHR",
         "AirlineCode": "BA",
         "completed_interviews": 19,
-        "download_time": "20-05-2026 22:56:38"
+        "download_time": "20-05-2026 23:23:46"
     },
     {
         "Interview_Date": "2023-01-21",
--- MUC_PEIAN/location_percent.js
+++ MUC_PEIAN/location_percent.js
@@ -5,7 +5,7 @@ let location_percent = `[
         "Location": "Unknown",
         "Exit_belt": "Unknown",
         "Percentage": 100,
-        "download_time": "20-05-2026 23:02:19"
+        "download_time": "20-05-2026 23:26:21"
     },
     {
         "Year": "2023",
