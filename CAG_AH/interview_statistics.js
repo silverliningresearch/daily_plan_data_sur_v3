12b37598fa6b2361868c75919811bbfbf17a6db0
unknown <lb@silverliningresearch.com>
2026-05-21 21:27:30 +0000

    update new data

--- CAG_AH/interview_statistics.js
+++ CAG_AH/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "TPE",
         "AirlineCode": "SQ",
         "completed_interviews": 1,
-        "download_time": "22-05-2026 04:58:07"
+        "download_time": "22-05-2026 05:24:27"
     },
     {
         "Interview_Date": "2025-01-01",
--- MUC_PEIAN/location_count.js
+++ MUC_PEIAN/location_count.js
@@ -5,7 +5,7 @@ let location_count = `[
         "Location": "Unknown",
         "Exit_belt": "Unknown",
         "completed_interviews": 6,
-        "download_time": "21-05-2026 23:03:33"
+        "download_time": "21-05-2026 23:26:50"
     },
     {
         "Year": "2023",
