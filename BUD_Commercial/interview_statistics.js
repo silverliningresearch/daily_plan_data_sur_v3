6f44b327845d65b02a5f75b2b6d490895200aa6f
unknown <lb@silverliningresearch.com>
2026-05-25 23:28:11 +0000

    update new data

--- BUD_Commercial/interview_statistics.js
+++ BUD_Commercial/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-01-02",
         "quota_id": "BCN-FR",
         "Number of interviews": 3,
-        "download_time": "26-05-2026 00:54:56"
+        "download_time": "26-05-2026 01:23:49"
     },
     {
         "InterviewDate": "2024-01-02",
--- MUC_PEIAN/location_percent.js
+++ MUC_PEIAN/location_percent.js
@@ -5,7 +5,7 @@ let location_percent = `[
         "Location": "Unknown",
         "Exit_belt": "Unknown",
         "Percentage": 100,
-        "download_time": "26-05-2026 01:01:24"
+        "download_time": "26-05-2026 01:27:40"
     },
     {
         "Year": "2023",
