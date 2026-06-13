eb442c3d8c8de96b494a63250914bb14de517774
unknown <lb@silverliningresearch.com>
2026-06-13 23:07:17 +0000

    update new data

--- AUH_DDS/interview_statistics.js
+++ AUH_DDS/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "Survey": 1,
         "Location": 8888888,
         "Number of interviews": 3,
-        "download_time": "14-06-2026 02:34:51"
+        "download_time": "14-06-2026 03:04:06"
     },
     {
         "InterviewDate": "2024-04-13",
--- MUC_PEIAN/location_count.js
+++ MUC_PEIAN/location_count.js
@@ -5,7 +5,7 @@ let location_count = `[
         "Location": "Unknown",
         "Exit_belt": "Unknown",
         "completed_interviews": 6,
-        "download_time": "14-06-2026 00:35:41"
+        "download_time": "14-06-2026 01:05:10"
     },
     {
         "Year": "2023",
