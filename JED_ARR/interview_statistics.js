0fb0b7a521f3c9b0e07e1bad49cdddef877f6ab9
unknown <lb@silverliningresearch.com>
2026-06-17 02:37:57 +0000

    update new data

--- JED_ARR/interview_statistics.js
+++ JED_ARR/interview_statistics.js
@@ -3,7 +3,7 @@ let interview_statistics = `[
         "InterviewDate": "2024-10-08",
         "quota_id": "SV-CAI",
         "Number of interviews": 3,
-        "download_time": "17-06-2026 05:00:58"
+        "download_time": "17-06-2026 05:36:30"
     },
     {
         "InterviewDate": "2024-10-08",
@@ -41273,7 +41273,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2026-06-17",
         "quota_id": "SV-DMM",
-        "Number of interviews": 4
+        "Number of interviews": 6
     },
     {
         "InterviewDate": "2026-06-17",
@@ -41283,7 +41283,7 @@ let interview_statistics = `[
     {
         "InterviewDate": "2026-06-17",
         "quota_id": "SV-RUH",
-        "Number of interviews": 3
+        "Number of interviews": 4
     },
     {
         "InterviewDate": "2026-06-17",
--- MUC_PEIAB/MUC_Gate_Info.js
+++ MUC_PEIAB/MUC_Gate_Info.js
@@ -8,7 +8,7 @@ let MUC_Gate_Info = `[
         "terminalIdentifier": "2",
         "GateArea": "T2 Main E4",
         "Gate": "G02",
-        "download_time": "17-06-2026 04:00:05"
+        "download_time": "17-06-2026 04:35:18"
     },
     {
         "aodbUniqueId": "20260617162925",
