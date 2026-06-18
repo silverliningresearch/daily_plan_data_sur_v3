16a19abb940510d01cedad1ea46e0da52de420b1
unknown <lb@silverliningresearch.com>
2026-06-18 06:54:45 +0000

    update new data

---
+++ Aalborg_Event/interview_statistics.js
@@ -0,0 +1,107 @@
+let interview_statistics = `[
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 1,
+        "Number of interviews": 7,
+        "download_time": "18-06-2026 07:24:55"
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 2,
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 3,
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 4,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 5,
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 6,
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 7,
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 8,
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 9,
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2026-06-17",
+        "InterviewerID": 10,
+        "Number of interviews": 5
+    },
+	{
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 1,
+        "Number of interviews": 7,
+        "download_time": "18-06-2026 07:24:55"
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 2,
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 3,
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 4,
+        "Number of interviews": 4
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 5,
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 6,
+        "Number of interviews": 12
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 7,
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 8,
+        "Number of interviews": 7
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 9,
+        "Number of interviews": 8
+    },
+    {
+        "InterviewDate": "2026-06-18",
+        "InterviewerID": 10,
+        "Number of interviews": 5
+    }
+	
+]    
+
+ `;
