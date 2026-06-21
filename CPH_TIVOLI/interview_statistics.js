e820b4fea9e2b5f13036bf070e477fbe281f61d4
unknown <lb@silverliningresearch.com>
2026-06-21 05:53:12 +0000

    update new data

--- CPH_TIVOLI/interview_statistics.js
+++ CPH_TIVOLI/interview_statistics.js
@@ -5,7 +5,7 @@ let interview_statistics = `[
         "AirlineCode": "AF",
         "NoOfItvs": 33,
         "completed_interviews": 33,
-        "download_time": "21-06-2026 07:28:49"
+        "download_time": "21-06-2026 07:53:10"
     },
     {
         "Interview_Date": "2026-06-21",
---
+++ CPH_WOCO/interview_statistics.js
@@ -0,0 +1,11 @@
+let interview_statistics = `[
+    {
+        "Interview_Date": "2026-06-21",
+        "Dest": "FCO",
+        "AirlineCode": "D8",
+        "completed_interviews": 1,
+        "download_time": "21-06-2026 07:53:12"
+    }
+]    
+
+ `;
