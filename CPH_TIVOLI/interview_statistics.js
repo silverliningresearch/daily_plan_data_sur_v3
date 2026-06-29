6a9bc9a12f9dfd0bf6ad06741d752e290affd717
unknown <lb@silverliningresearch.com>
2026-06-29 13:27:27 +0000

    update new data

--- CPH_TIVOLI/interview_statistics.js
+++ CPH_TIVOLI/interview_statistics.js
@@ -1,25 +1,18 @@
 let interview_statistics = `[
     {
-        "Interview_Date": "2026-06-21",
-        "Dest": "CDG",
-        "AirlineCode": "AF",
-        "NoOfItvs": 33,
-        "completed_interviews": 33,
-        "download_time": "29-06-2026 15:01:59"
-    },
-    {
-        "Interview_Date": "2026-06-21",
-        "Dest": "MUC",
-        "AirlineCode": "LH",
-        "NoOfItvs": 36,
-        "completed_interviews": 36
-    },
-    {
         "Interview_Date": "2026-06-29",
         "Dest": "BCN",
         "AirlineCode": "SK",
         "NoOfItvs": 2,
-        "completed_interviews": 2
+        "completed_interviews": 2,
+        "download_time": "29-06-2026 15:27:26"
+    },
+    {
+        "Interview_Date": "2026-06-29",
+        "Dest": "BOD",
+        "AirlineCode": "SK",
+        "NoOfItvs": 1,
+        "completed_interviews": 1
     }
 ]    
 
