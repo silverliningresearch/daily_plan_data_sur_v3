3ffd142ab072be34c1708670240997e99dbd9894
unknown <lb@silverliningresearch.com>
2026-09-16 09:49:45 +0000

    update new data

---
+++ MUC_AXA/interview_statistics.js
@@ -0,0 +1,20 @@
+let interview_statistics_arr = `[
+    {
+        "Interview_Date": "2026-09-16",
+        "Location": 1,
+        "completed_interviews": 1,
+        "download_time": "16-09-2026 11:42:46"
+    },
+    {
+        "Interview_Date": "2026-09-16",
+        "Location": 2,
+        "completed_interviews": 2
+    },
+    {
+        "Interview_Date": "2026-09-16",
+        "Location": "",
+        "completed_interviews": 1
+    }
+]    
+
+ `;
---
+++ MUC_AXA/location_count.js
@@ -0,0 +1,23 @@
+let location_count = `[
+    {
+        "Year": "2026",
+        "Month": "09",
+        "Location": 1,
+        "completed_interviews": 1,
+        "download_time": "16-09-2026 11:42:46"
+    },
+    {
+        "Year": "2026",
+        "Month": "09",
+        "Location": 2,
+        "completed_interviews": 2
+    },
+    {
+        "Year": "2026",
+        "Month": "09",
+        "Location": "",
+        "completed_interviews": 1
+    }
+]    
+
+ `;
