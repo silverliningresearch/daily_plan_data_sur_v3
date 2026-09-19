aca7d01ad83ed7b2ddd3f610016a9e4b07d9e4cf
unknown <lb@silverliningresearch.com>
2026-09-19 02:44:01 +0000

    update new data

--- MUC_AXA/interview_statistics.js
+++ MUC_AXA/interview_statistics.js
@@ -2,26 +2,26 @@ let interview_statistics_arr = `[
     {
         "Interview_Date": "2026-09-17",
         "Location": 2,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 11,
-        "download_time": "19-09-2026 02:09:21"
+        "download_time": "19-09-2026 04:44:01"
     },
     {
         "Interview_Date": "2026-09-18",
         "Location": 1,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 19
     },
     {
         "Interview_Date": "2026-09-18",
         "Location": 2,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 28
     },
     {
         "Interview_Date": "2026-09-18",
         "Location": 3,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 7
     }
 ]    
--- MUC_AXA/location_count.js
+++ MUC_AXA/location_count.js
@@ -3,22 +3,22 @@ let location_count = `[
         "Year": "2026",
         "Month": "09",
         "Location": 1,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 19,
-        "download_time": "19-09-2026 02:09:21"
+        "download_time": "19-09-2026 04:44:01"
     },
     {
         "Year": "2026",
         "Month": "09",
         "Location": 2,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 39
     },
     {
         "Year": "2026",
         "Month": "09",
         "Location": 3,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 7
     }
 ]    
