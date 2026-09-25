5788beefe9b55d8f84fb209d2891aff2a2d57e72
unknown <lb@silverliningresearch.com>
2026-09-25 16:09:12 +0000

    update new data

--- MUC_AXA/location_count.js
+++ MUC_AXA/location_count.js
@@ -3,22 +3,22 @@ let location_count = `[
         "Year": "2026",
         "Month": "09",
         "Location": 1,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 97,
-        "download_time": "25-09-2026 17:54:08"
+        "download_time": "25-09-2026 18:09:12"
     },
     {
         "Year": "2026",
         "Month": "09",
         "Location": 2,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 91
     },
     {
         "Year": "2026",
         "Month": "09",
         "Location": 3,
-        "Wave": 1.0,
+        "Wave": 1,
         "completed_interviews": 76
     }
 ]    
